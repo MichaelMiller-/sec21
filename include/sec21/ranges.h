eb3e4dcd8480a87e59a9de1dadeb2f4a84de79d1
MichaelMiller- <miller.michael@gmx.de>
2018-02-21 16:33:17 +0100

    setup repo

--- include/sec21/ranges.h
+++ include/sec21/ranges.h
@@ -3,7 +3,7 @@
 #include <range/v3/all.hpp>
 
 namespace sec21 {
-
+   //! \todo intersect is NOT tested
    //! \brief create a intersection of two sets A and B
    //! \link https://en.wikipedia.org/wiki/Intersection_(set_theory)
    //! \param v range of elements to examine 
@@ -11,7 +11,7 @@ namespace sec21 {
    auto intersect(T && v) {
       return ranges::view::filter([&v](auto && a) {
          return ranges::distance(v | ranges::view::filter([a](auto && b) { return a == b; })) == 1; });
-   };
+   }
 
    //! \brief create a symmetric difference of two sets A and B
    //! \link https://en.wikipedia.org/wiki/Symmetric_difference
@@ -20,6 +20,6 @@ namespace sec21 {
    auto difference(T && v) {
       return ranges::view::remove_if([&v](auto && a) {
          return !ranges::empty(v | ranges::view::filter([&a](auto && b) { return a == b; })); });
-   };
+   }
 
 }
