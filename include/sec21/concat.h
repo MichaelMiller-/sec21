941174d07c2e165f1e254d2b2a8ff8f62cafe0d4
michael miller <miller.michael@gmx.de>
2018-06-25 17:09:19 +0200

    mispelling

--- include/sec21/concat.h
+++ include/sec21/concat.h
@@ -19,7 +19,7 @@ namespace sec21
    }
 
    //! \brief join string-convertible objects into one string
-   // eg: std::string s = sof::concat<char>("foo ", 42, " bar ", streamable_object, "value2");
+   // eg: std::string s = sec21::concat<char>("foo ", 42, " bar ", streamable_object, "value2");
    template <typename CharT, typename... Args>
    std::basic_string<CharT> concat(Args&&... args)
    {
