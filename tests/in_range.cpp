f015049c4e839db0e190676917628ed9be1a7931
Miller <mm@nbg.sofistik.de>
2018-08-14 09:42:06 +0200

    remove warning

--- tests/in_range.cpp
+++ tests/in_range.cpp
@@ -7,19 +7,19 @@
 // compiletime tests
 //
 // test with ints
-static_assert(sec21::in_range(7, 1, 10));
-static_assert(sec21::in_range(1, 1, 10));
-static_assert(sec21::in_range(10, 1, 10));
+static_assert(sec21::in_range(7, 1, 10), "7 should be in range of 1 to 10");
+static_assert(sec21::in_range(1, 1, 10), "1 should be in range of 1 to 10");
+static_assert(sec21::in_range(10, 1, 10), "10 should be in range of 1 to 10");
 
-static_assert(!sec21::in_range(-7, 1, 10));
-static_assert(!sec21::in_range(-1, 1, 10));
-static_assert(!sec21::in_range(11, 1, 10));
+static_assert(!sec21::in_range(-7, 1, 10), "-7 shouldn't be in range of 1 to 10");
+static_assert(!sec21::in_range(-1, 1, 10), "-1 shouldn't be in range of 1 to 10");
+static_assert(!sec21::in_range(11, 1, 10), "11 shouldn't be in range of 1 to 10");
 
 // test with doubles
-static_assert(sec21::in_range(7.0, 1.0, 10.0));
-static_assert(sec21::in_range(1.0, 1.0, 10.0));
-static_assert(sec21::in_range(10.0, 1.0, 10.0));
+static_assert(sec21::in_range(7.0, 1.0, 10.0), "7.0 should be in range of 1.0 to 10.0");
+static_assert(sec21::in_range(1.0, 1.0, 10.0), "1.0 should be in range of 1.0 to 10.0");
+static_assert(sec21::in_range(10.0, 1.0, 10.0), "10.0 should be in range of 1.0 to 10.0");
 
-static_assert(!sec21::in_range(-7.0, 1.0, 10.0));
-static_assert(!sec21::in_range(-1.0, 1.0, 10.0));
-static_assert(!sec21::in_range(11.0, 1.0, 10.0));
+static_assert(!sec21::in_range(-7.0, 1.0, 10.0), "-7.0 shouldn't be in range of 1.0 to 10.0");
+static_assert(!sec21::in_range(-1.0, 1.0, 10.0), "-1.0 shouldn't be in range of 1.0 to 10.0");
+static_assert(!sec21::in_range(11.0, 1.0, 10.0), "11.0 shouldn't be in range of 1.0 to 10.0");
