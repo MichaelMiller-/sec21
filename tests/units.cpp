832bceec69783dac9cd6ba36cbd9591e0aeae316
Michael <miller.michael@gmx.de>
2018-12-05 00:51:16 +0100

    tests depends on boost version

--- tests/units.cpp
+++ tests/units.cpp
@@ -30,11 +30,16 @@ static_assert(r1 == 207_mm, "must be equal");
 
 static_assert(m2 - m1 == m1, "must be equal");
 
-//! \todo might be a problem with old boost version 1.54 mpl::transform
-//static_assert(r2 == su::SquareMeter<int>(1), "must be equal");
-//static_assert((su::Foot<int>{3} * su::Foot<int>{4}) == su::SquareFoot<int>{12}, "must be equal");
+#include <boost/version.hpp>
+
+#if BOOST_VERSION >= 106700
+
+static_assert(r2 == su::SquareMeter<int>(1), "must be equal");
+static_assert((su::Foot<int>{3} * su::Foot<int>{4}) == su::SquareFoot<int>{12}, "must be equal");
 
 static_assert((m1 * m1 * m1) == su::CubicMeter<int>(1), "must be equal");
+
+#endif
 
 static_assert((m1 / m1) == su::Id<int>(1), "must be equal");
 
