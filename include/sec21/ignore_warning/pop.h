11e9d9a56114fd6856c85a7af81cf78bfc93df95
Michael <miller.michael@gmx.de>
2019-04-26 14:35:11 +0200

    header to disable specific warnings

---
+++ include/sec21/ignore_warning/pop.h
@@ -0,0 +1,7 @@
+#if defined(__clang__)
+#  pragma clang diagnostic pop
+#endif
+
+#if defined(_MSC_VER)
+#  pragma warning(pop)
+#endif
---
+++ include/sec21/ignore_warning/push.h
@@ -0,0 +1,7 @@
+#if defined(__clang__)
+#  pragma clang diagnostic push
+#endif
+
+#if defined(_MSC_VER)
+#  pragma warning(push)
+#endif
