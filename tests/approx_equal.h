f1c804004c8f5f864849044bc2b5bfd7b6674b73
Michael <miller.michael@gmx.de>
2019-04-26 14:28:56 +0200

    helper to compare sequences

---
+++ tests/approx_equal.h
@@ -0,0 +1,21 @@
+﻿#include "catch.hpp"
+
+#include <algorithm>
+
+// Vergleich mit einer n-Prozent Abweichung
+template <typename Iterator1, typename Iterator2>
+bool approx_equal(Iterator1 first1, Iterator1 last1, Iterator2 first2, double divergence) noexcept
+{
+   return std::equal(
+      first1, 
+      last1, 
+      first2, 
+      [divergence](auto const& lhs, auto const& rhs) { return Approx(lhs).epsilon(divergence) == rhs; }); 
+}
+
+// Vergleich mit einer n-Prozent Abweichung
+template <typename Rng1, typename Rng2>
+bool approx_equal(Rng1 const& rng1, Rng2 const& rng2, double divergence) noexcept
+{
+   return approx_equal(std::begin(rng1), std::end(rng1), std::begin(rng2), divergence);
+}
