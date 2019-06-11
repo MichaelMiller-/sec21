be43502426141f36c284986bb0e726a60d2a3cc5
michael miller <miller.michael@gmx.de>
2019-06-11 20:29:43 +0200

    fix concept requirement

--- tests/perfect.forwarding.cpp
+++ tests/perfect.forwarding.cpp
@@ -19,7 +19,7 @@ public:
 #ifndef __cpp_concepts
    , typename = std::enable_if_t<std::is_convertible_v<T, std::string>>>
 #else   
-   > requires std::is_constructible_v<T, std::string>
+   > requires std::is_constructible_v<std::string, T>
 #endif
    Customer(T&& first_, U&& last_ = "", int id_ = 0) 
       : first{ std::forward<T>(first_) }
@@ -37,29 +37,31 @@ class VIP : public Customer
 
 TEST_CASE("perfect forwarding", "[compilation-only]") 
 {
-   Customer foo_bar("foo", "bar", 42);             // 2 malloc ( 2 creation(cr) )
+   using namespace std::literals::string_literals;
+
+   Customer foo_bar("foo"s, "bar"s, 42);             // 2 malloc ( 2 creation(cr) )
    {
-      std::string s = "baz";
-      Customer baz_bar(s, "bar", 42);              // 2 malloc ( 1 cp + 2 cr )
-      Customer baz_baz(std::move(s), "baz", 42);   // 1 malloc ( 1 cr + 1 mv )
-      Customer only_foo("foo");
+      std::string s{ "baz" };                       // 1 malloc                       <--------
+      Customer baz_bar(s, "bar"s, 42);              // 2 malloc ( 1 cp + 2 cr )               |
+      Customer baz_baz(std::move(s), "baz"s, 42);   // 1 malloc ( 1 cr + 1 mv )  + (1 malloc) |
+      Customer only_foo("foo"s);
 
       Customer copy_foo_bar( foo_bar );
-      const Customer const_c( "c" );
+      const Customer const_c( "c"s );
       Customer copy_c( const_c );
 
-      Customer assign_c = "c";
+      Customer assign_c = "c"s;
    }
    {
-      std::string s = "baz";
-      VIP baz_bar( s, "bar", 42 );
-      VIP baz_baz( std::move(s), "baz", 42 );
-      VIP only_foo( "foo" );
+      std::string s{ "baz" };
+      VIP baz_bar( s, "bar"s, 42 );
+      VIP baz_baz( std::move(s), "baz"s, 42 );
+      VIP only_foo( "foo"s );
 
-      const VIP const_c( "c" );
+      const VIP const_c( "c"s );
       VIP copy_c( const_c );
 
-      VIP assign_c = "c";
+      VIP assign_c = "c"s;
    
       Customer copy_vip( baz_bar );
    }
