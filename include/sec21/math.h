80431973f499002e879e2ffdcffae66d70c4dc90
Miller <mm@nbg.sofistik.de>
2019-08-12 14:40:41 +0200

    add accumulate

--- include/sec21/math.h
+++ include/sec21/math.h
@@ -6,13 +6,33 @@
 
 namespace sec21
 {
+#ifdef __cpp_fold_expressions
+   
+   //! \brief
+   template <typename... Args>
+   auto accumulate(Args ... args) noexcept
+   {
+      return (args + ...);
+   }
+
+   //! \brief
    template <typename... Args>
    auto average(Args &&... args) noexcept
    {
-      return (std::forward<Args>(args) + ...) / sizeof...(Args);
+      return accumulate(std::forward<Args>(args)...) / sizeof...(Args);
    }
 
-   //! calculates the absolute norm of an array (Euklidische Norm)
+   //! \brief
+   template <typename Arithmetic, typename... Args>
+   decltype(auto) fmin(Arithmetic v, Args &&... args)
+   {
+      auto result{ v };
+      ((result = std::fmin(result, args)), ...);
+      return result;
+   }
+#endif
+
+   //! \brief calculates the absolute norm of an array (Euklidische Norm)
    template <typename Iterator>
    constexpr auto norm(Iterator first, Iterator last) noexcept
    {
@@ -23,12 +43,4 @@ namespace sec21
               typename std::iterator_traits<Iterator>::value_type(), 
               [](auto const& a, auto const& b){ return a + (b * b); }));
    }
-
-   template <typename Arithmetic, typename... Args>
-   decltype(auto) fmin(Arithmetic v, Args &&... args)
-   {
-      auto result{ v };
-      ((result = std::fmin(result, args)), ...);
-      return result;
-   }   
 }
