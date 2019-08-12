b9f8604ae418d8384094c462625bf3b861c5203b
MichaelMiller- <miller.michael@gmx.de>
2019-08-12 15:51:39 +0200

    add trailing return types

--- include/sec21/double_values.h
+++ include/sec21/double_values.h
@@ -6,8 +6,9 @@
 
 namespace sec21
 {
+   //! \todo 2019-05-14 result_t and vector return_type
    template <size_t N, typename T, typename Iterator, typename TransformFunc>
-   [[nodiscard]] auto double_values(Iterator first, Iterator last, TransformFunc func)
+   [[nodiscard]] auto double_values(Iterator first, Iterator last, TransformFunc func) noexcept -> std::vector<T>
    {
       const auto s{ std::distance(first, last) };
       std::vector<T> result(s * N);
@@ -23,18 +24,12 @@ namespace sec21
       return result;
    }
 
-   //! \brief doubles the value in the input sequence N-times eg: N=2; {1,2,3} -> {1,1,2,2,3,3}
+   //! \brief doubles the value in the input sequence N-times eg: N=3; {1,2,3} -> {1,1,1,2,2,2,3,3,3}
    template <size_t N, typename Iterator>
-   [[nodiscard]] auto double_values(Iterator first, Iterator last)
+   [[nodiscard]] auto double_values(Iterator first, Iterator last) noexcept -> std::vector<typename std::iterator_traits<Iterator>::value_type>
    {
       using T = typename std::iterator_traits<Iterator>::value_type;
       return double_values<N, T>(first, last, [](auto && e){ return e; });
-   }
-
-   template <size_t N, typename Container>
-   [[nodiscard]] auto double_values(Container const& c) 
-   { 
-      return double_values(std::begin(c), std::end(c)); 
    }
 }
 
