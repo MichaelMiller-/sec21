8f89d6c0b33c8ce4ea803ea7b411f97359fc7631
michael miller <miller.michael@gmx.de>
2019-10-05 18:07:53 +0200

    remove date

---
+++ deps/viennacl/build/libviennacl/CMakeFiles/viennacl.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/viennacl.dir/src/backend.cpp.o"
+  "CMakeFiles/viennacl.dir/src/blas1.cpp.o"
+  "CMakeFiles/viennacl.dir/src/blas1_host.cpp.o"
+  "CMakeFiles/viennacl.dir/src/blas1_opencl.cpp.o"
+  "CMakeFiles/viennacl.dir/src/blas2.cpp.o"
+  "CMakeFiles/viennacl.dir/src/blas2_host.cpp.o"
+  "CMakeFiles/viennacl.dir/src/blas2_opencl.cpp.o"
+  "CMakeFiles/viennacl.dir/src/blas3.cpp.o"
+  "CMakeFiles/viennacl.dir/src/blas3_host.cpp.o"
+  "CMakeFiles/viennacl.dir/src/blas3_opencl.cpp.o"
+  "libviennacl.pdb"
+  "libviennacl.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/viennacl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
