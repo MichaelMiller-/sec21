11eb46d58998bf148a8aacc76c588bc2d5f78ca5
michael miller <miller.michael@gmx.de>
2019-11-17 18:14:43 +0100

    inital commit

---
+++ include/sec21/contains.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <algorithm>
+
+namespace sec21
+{
+   template <typename T>
+   constexpr bool contains(T const& t, typename T::value_type const& v) noexcept
+   {
+      return std::find(std::begin(t), std::end(t), v) != std::end(t);
+   }
+}
