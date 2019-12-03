139029f59fa28f4c0fdd710adca10ab72d8288bc
MichaelMiller- <miller.michael@gmx.de>
2019-12-03 14:14:59 +0100

    force is now a alias for std::array

--- include/sec21/structural_analysis/force.h
+++ include/sec21/structural_analysis/force.h
@@ -3,108 +3,39 @@
 #include <sec21/units.h>
 
 #include <array>
-#include <algorithm>
 #include <type_traits>
 
 namespace sec21::structural_analysis
 {
-   template <std::size_t Dimension, typename Unit = unit::kilonewton<double>>
-//! \todo concept
-#ifdef __cpp_concepts
-   requires Force<Unit>
-#endif
-   class force
+   template <size_t Dimension>
+   using force_t = std::array<unit::kilonewton<double>, Dimension>;
+
+   using force_2D_t = force_t<2>;
+   using force_3D_t = force_t<3>;
+
+   template <size_t Dimension>
+   [[nodiscard]] constexpr auto operator + (force_t<Dimension> const& lhs, force_t<Dimension> const& rhs) noexcept -> force_t<Dimension>
    {
-   public:
-      static constexpr auto dimension_v = Dimension;
-      static_assert(Dimension == 2 || Dimension == 3, "Only works in 2D and 3D");
-
-   private:
-      using value_t = std::array<Unit, Dimension>;
-      value_t value;
-
-   public:
-      // template <typename... Args>
-      // constexpr force(Args &&... args) noexcept 
-      //    : value{ std::forward<Args>(args)... } 
-      // {
-      //    static_assert(sizeof...(args) < Dimension + 1, "too many arguments for this class");
-      // }
-
-      // template <typename T>
-// #ifndef __cpp_concepts
-//       , typename = std::enable_if_t<std::is_convertible_v<T, value_t>>>
-// #else   
-//       > requires std::is_constructible_v<T, value_t>
-// #endif
-      constexpr force(value_t && v) noexcept 
-         : value{ std::move(v) } 
-      {
-      }        
-
-      constexpr auto begin() const noexcept
-      {
-         return std::begin(value); 
+      if constexpr (Dimension == 2) {
+         return { { std::get<0>(lhs) + std::get<0>(rhs), std::get<1>(lhs) + std::get<1>(rhs) } };
       }
-      constexpr auto end() const noexcept 
-      { 
-         return std::end(value); 
-      }
-
-      [[nodiscard]] friend constexpr auto operator + (force<Dimension> const& lhs, force<Dimension> const& rhs) noexcept -> force<Dimension>
-      {
-         if constexpr (Dimension == 2) {
-            return { { 
-               std::get<0>(lhs.value) + std::get<0>(rhs.value),
-               std::get<1>(lhs.value) + std::get<1>(rhs.value)
-            } };
-         }
-         if constexpr (Dimension == 3) {
-            return { { 
-               std::get<0>(lhs.value) + std::get<0>(rhs.value),
-               std::get<1>(lhs.value) + std::get<1>(rhs.value),
-               std::get<2>(lhs.value) + std::get<2>(rhs.value)
-            } };
-         }
-      }
-
-#ifdef __cpp_lib_constexpr_misc
-      [[nodiscard]] friend constexpr bool operator == (force<Dimension> const& lhs, force<Dimension> const& rhs) noexcept {
-         return std::equal(std::begin(lhs.value), std::end(lhs.value), std::begin(rhs.value));
-      }
-      [[nodiscard]] friend constexpr bool operator != (force<Dimension> const& lhs, force<Dimension> const& rhs) noexcept {
-         return !(lhs == rhs);
-      }
-#else
-      [[nodiscard]] friend bool operator == (force<Dimension> const& lhs, force<Dimension> const& rhs) noexcept {
-         return std::equal(std::begin(lhs.value), std::end(lhs.value), std::begin(rhs.value));
-      }
-      [[nodiscard]] friend bool operator != (force<Dimension> const& lhs, force<Dimension> const& rhs) noexcept {
-         return !(lhs == rhs);
-      }
-#endif
-   };
-
-   namespace detail 
-   {
-      template <typename T>
-      constexpr auto null_force() noexcept -> T
-      {
-         constexpr auto dim_v = T::dimension_v;
-
-         if constexpr (dim_v == 2) {
-            return { { 0.0, 0.0 } };
-         }
-         if constexpr (dim_v == 3) {
-            return { { 0.0, 0.0, 0.0 } };
-         }
+      if constexpr (Dimension == 3) {
+         return { { 
+            std::get<0>(lhs) + std::get<0>(rhs),
+            std::get<1>(lhs) + std::get<1>(rhs),
+            std::get<2>(lhs) + std::get<2>(rhs)
+         } };
       }
    }
 
    template <typename Iterator>
    [[nodiscard]] auto superposition(Iterator first, Iterator last) noexcept
    {
-      auto result = detail::null_force<typename std::iterator_traits<Iterator>::value_type>();
-      return std::accumulate(first, last, result);
+      typename std::iterator_traits<Iterator>::value_type result{};
+
+      for (auto it = first; it != last; ++it)
+         result = result + *it;
+      //! \todo 2019-08-08: is not working: return std::reduce(first, last);
+      return result;
    }
 }
--- tests/structural_analysis.force.cpp
+++ tests/structural_analysis.force.cpp
@@ -3,20 +3,16 @@
 
 #include <sec21/structural_analysis/force.h>
 
-
 TEST_CASE("2D force", "[force]")
 {
    using namespace sec21::structural_analysis;
    using namespace sec21::unit::literals;
 
-   using force_t = force<2>;
+   using force_t = force_2D_t;
 
-   constexpr force_t f1{ { 3.0_kN, 1.0_kN } };
-   constexpr force_t f2{ { 0.0, 10.0 } };
-   constexpr force_t f3{ { 10.0_N, 4.0_MN } };
-
-   //! \todo 
-   //static_assert(f1 == f1, "must be equal");
+   const force_t f1{ { 3.0_kN, 1.0_kN } };
+   const force_t f2{ { 0.0, 10.0 } };
+   const force_t f3{ { 10.0_N, 4.0_MN } };
 
    SECTION("equal test")
    {
@@ -38,7 +34,7 @@ TEST_CASE("2D force", "[force]")
    }
    SECTION("superposition principle from empty input")
    {
-      std::vector<force_t> input{};
+      std::vector<force_t> input;
       const auto expected = force_t{ { 0.0, 0.0 } };
       REQUIRE(superposition(std::begin(input), std::end(input)) == expected);
    }
@@ -49,16 +45,17 @@ TEST_CASE("2D force", "[force]")
       REQUIRE(superposition(std::begin(input), std::end(input)) == expected);
    }
 }
+
 TEST_CASE("3D force", "[force]")
 {
    using namespace sec21::structural_analysis;
    using namespace sec21::unit::literals;
 
-   using force_t = force<3>;
+   using force_t = force_3D_t;
 
-   constexpr force_t f1{ {3.0_kN, 1.0, 2.0_kN} };
-   constexpr force_t f2{ {0.0, 10.0, 0.0} };
-   constexpr force_t f3{ {10.0_N, 4.0_MN, 0.3_MN} };
+   const force_t f1{ { 3.0_kN, 1.0_kN, 5.2_kN } };
+   const force_t f2{ { 0.0, 10.0, 0.0 } };
+   const force_t f3{ { 10.0_N, 4.0_MN, 42.3_kN } };
 
    SECTION("equal test")
    {
@@ -71,23 +68,23 @@ TEST_CASE("3D force", "[force]")
    }
    SECTION("addition")
    {
-      REQUIRE((f1 + f1) == force_t{ {6.0_kN, 2.0_kN, 4.0_kN} });
-      REQUIRE((f2 + f2) == force_t{ {0.0_kN, 20.0_kN, 0.0_kN} });
-      REQUIRE((f3 + f3) == force_t{ {0.020_kN, 8000.0_kN, 600.0_kN} });
-      REQUIRE((f1 + f2) == force_t{ {3.0_kN, 11.0_kN, 2.0_kN} });
-      REQUIRE((f1 + f3) == force_t{ {3.010_kN, 4001.0_kN, 302.0_kN} });
-      REQUIRE((f2 + f3) == force_t{ {0.010_kN, 4010.0_kN, 300.0_kN} });
+      REQUIRE((f1 + f1) == force_t{ { 6.0_kN, 2.0_kN, 10.4_kN } });
+      REQUIRE((f2 + f2) == force_t{ { 0.0_kN, 20.0_kN, 0.0 } });
+      REQUIRE((f3 + f3) == force_t{ { 0.020_kN, 8000.0_kN, 84.6_kN } });
+      REQUIRE((f1 + f2) == force_t{ { 3.0_kN, 11.0_kN, 5.2_kN } });
+      REQUIRE((f1 + f3) == force_t{ { 3.010_kN, 4001.0_kN, 47.5_kN } });
+      REQUIRE((f2 + f3) == force_t{ { 0.010_kN, 4010.0_kN, 42.3_kN } });
    }
    SECTION("superposition principle from empty input")
    {
-      std::vector<force_t> input{};
-      const auto expected = force_t{ {0.0, 0.0, 0.0} };
+      std::vector<force_t> input;
+      const auto expected = force_t{ { 0.0, 0.0, 0.0 } };
       REQUIRE(superposition(std::begin(input), std::end(input)) == expected);
    }
    SECTION("superposition principle")
    {
       std::vector<force_t> input{ f1, f2, f3 };
-      const auto expected = force_t{ {3.010_kN, 4011.0_kN, 302.0_kN} };
+      const auto expected = force_t{ { 3.010_kN, 4011.0_kN, 47.5_kN } };
       REQUIRE(superposition(std::begin(input), std::end(input)) == expected);
    }
 }
