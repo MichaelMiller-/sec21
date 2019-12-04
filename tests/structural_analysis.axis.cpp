2b1ee27f0b8de6d010f5ee46a28a6a17b1684886
MichaelMiller- <miller.michael@gmx.de>
2019-12-04 11:28:27 +0100

    code format

--- tests/structural_analysis.axis.cpp
+++ tests/structural_analysis.axis.cpp
@@ -8,3 +8,9 @@ using namespace sec21::structural_analysis;
 static_assert(static_cast<int>(Axis::X) == 0, "Value of X-Axis has to be 0");
 static_assert(static_cast<int>(Axis::Y) == 1, "Value of Y-Axis has to be 1");
 static_assert(static_cast<int>(Axis::Z) == 2, "Value of Z-Axis has to be 2");
+
+TEST_CASE("structural analysis axis", "[compilation-only]") 
+{
+   SUCCEED("Nothing to test. Compiletests");
+}
+
