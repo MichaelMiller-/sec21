1193746f2e389f63a884739a69b5437152f49ae7
michael miller <miller.michael@gmx.de>
2020-02-14 23:17:57 +0100

    initial commit

---
+++ tests/structural_analysis.lookup.cpp
@@ -0,0 +1,30 @@
+﻿#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <sec21/structural_analysis/impl/lookup.h>
+
+#include <vector>
+#include <algorithm>
+
+TEST_CASE("row_to_col()", "[sec21][structural_analysis_impl][space_truss]")
+{
+   using namespace sec21::structural_analysis::impl;
+
+   SECTION("empty rows")
+   {
+      const auto input = std::vector<Row>{};
+      auto result = row_to_col(std::begin(input), std::end(input));
+      REQUIRE(std::empty(result));
+   }
+   SECTION("example row list")
+   {
+      std::vector<Row> input(5);
+      std::iota(std::begin(input), std::end(input), Row{0});
+      auto result = row_to_col(std::begin(input), std::end(input));
+
+      static_assert(std::is_same_v<Col, decltype(result)::value_type>);
+      const auto expected = std::array{ Col{0}, Col{1}, Col{2}, Col{3}, Col{4} };
+      REQUIRE(std::size(result) == std::size(expected));
+      REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)));
+   }
+}
