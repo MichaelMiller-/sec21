635d28096060dad4d339d21148c1f81942269e61
michael miller <miller.michael@gmx.de>
2020-03-30 21:31:36 +0200

    replace static_assert with STATIC_REQUIRE

--- tests/any_of.cpp
+++ tests/any_of.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include <catch.hpp>
 
 #include <tuple>
 #include <functional>
@@ -26,25 +26,9 @@ namespace sec21
 
    template <typename... Ts>
    any_of(Ts &&...)->any_of<Ts...>;
-
-   // template <typename CompareOperator, typename LogicOperator, typename... Ts>
-   // struct generic_logic : std::tuple<Ts...>
-   // {
-   //     template <typename U>
-   //     constexpr bool operator ()(U const& u) const noexcept
-   //     {
-   //         // LogicOperator(CompareOperator(v, u), ...)
-   //         return std::apply([&](auto const& ... v) { return (CompareOperator(v, u) || ...);}, get());
-   //     }
-   // private:
-   //     constexpr std::tuple<Ts...> const& get() const noexcept
-   //     {
-   //         return *this;
-   //     }
-   // };
 }
 
-TEST_CASE("syntax helper", "[sec21][compilation-only]")
+TEST_CASE("syntax helper", "[sec21][core]")
 {
    using namespace sec21;
 
@@ -52,11 +36,9 @@ TEST_CASE("syntax helper", "[sec21][compilation-only]")
    constexpr int y{50};
    constexpr int z{99};
 
-   static_assert(any_of{x, y, z} < 100);
-   static_assert(any_of{x, y, z} < 50);
-   static_assert((any_of{x, y, z} < 10) == false);
-   static_assert((any_of{x, y, z} < 9) == false);
-   static_assert((any_of{x, y, z} < 1) == false);
-
-   SUCCEED("Nothing to test. Compiletime-Tests");
+   STATIC_REQUIRE(any_of{x, y, z} < 100);
+   STATIC_REQUIRE(any_of{x, y, z} < 50);
+   STATIC_REQUIRE((any_of{x, y, z} < 10) == false);
+   STATIC_REQUIRE((any_of{x, y, z} < 9) == false);
+   STATIC_REQUIRE((any_of{x, y, z} < 1) == false);
 }
--- tests/combine.cpp
+++ tests/combine.cpp
@@ -1,11 +1,9 @@
-﻿#define _SILENCE_CXX17_UNCAUGHT_EXCEPTION_DEPRECATION_WARNING
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+﻿#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
 
 #include <sec21/combine.h>
 
-// namespaces are only for convenience
-namespace test_case_all_of
+TEST_CASE("combine lambdas; all_of", "[sec21][core]") 
 {
    constexpr auto lambda1 = [](auto e){ return e < 100; };
    constexpr auto lambda2 = [](auto e){ return e > 5; };
@@ -13,12 +11,12 @@ namespace test_case_all_of
 
    auto f1 = sec21::all_of(lambda1, lambda2, lambda3);
 
-   static_assert(f1(6) == false, "should be false");
-   static_assert(f1(16) == true, "should be true");
-   static_assert(f1(116) == false, "should be false");
+   STATIC_REQUIRE(f1(6) == false);
+   STATIC_REQUIRE(f1(16) == true);
+   STATIC_REQUIRE(f1(116) == false);
 }
 
-namespace test_case_any_of
+TEST_CASE("combine lambdas; any_of", "[sec21][core]") 
 {
    constexpr auto lambda1 = [](auto e){ return e == 100; };
    constexpr auto lambda2 = [](auto e){ return e == 5; };
@@ -26,14 +24,10 @@ namespace test_case_any_of
 
    auto f1 = sec21::any_of(lambda1, lambda2, lambda3);
 
-   static_assert(f1(6) == false, "should be false");
-   static_assert(f1(5) == true, "should be true");
-   static_assert(f1(10) == true, "should be true");
-   static_assert(f1(15) == false, "should be false");
-   static_assert(f1(100) == true, "should be true");
-   static_assert(f1(116) == false, "should be false");
-}
-
-TEST_CASE("combine", "[compilation-only]") {
-   SUCCEED("Nothing to test. Compiletests");
+   STATIC_REQUIRE(f1(6) == false);
+   STATIC_REQUIRE(f1(5) == true);
+   STATIC_REQUIRE(f1(10) == true);
+   STATIC_REQUIRE(f1(15) == false);
+   STATIC_REQUIRE(f1(100) == true);
+   STATIC_REQUIRE(f1(116) == false);
 }
