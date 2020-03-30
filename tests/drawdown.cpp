1d0caf556ed8056a803f90f50111029061bbf22d
michael miller <miller.michael@gmx.de>
2020-03-30 20:44:01 +0200

    fix type conversion

--- tests/drawdown.cpp
+++ tests/drawdown.cpp
@@ -1,9 +1,5 @@
-﻿//! \todo warnings produced from boost::numeric::ublas
-//#define _SILENCE_CXX17_OLD_ALLOCATOR_MEMBERS_DEPRECATION_WARNING
-//#define _SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING
-
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+﻿#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
 #include "approx_equal.h"
 
 #include <vector>
@@ -18,7 +14,7 @@ namespace sec21
    auto max_drawdown(Iterator first, Iterator last)
    {
       using value_t = typename std::iterator_traits<Iterator>::value_type;
-      const auto n = std::distance(first, last);
+      const size_t n = std::distance(first, last);
  
       std::vector<value_t> maxs(n);
       std::partial_sum(first, last, std::begin(maxs), [](auto l, auto r) { return std::max(l, r); });
@@ -33,27 +29,11 @@ namespace sec21
    }
 }
 
-/*
-float MaxDrawdown(const vector<int>& stock)
-{
-    auto maxs = view::partial_sum(stock, [](auto l, auto r){ return std::max(l, r); });
-    auto dds = view::zip_with([](auto peak, auto ith) { return (float)(peak-ith)/peak; }, maxs, stock);
-    return 100.0f * ranges::max(dds);
-}
-*/
-      // -> zip | map | reduce
-
-TEST_CASE("add node to system", "[drawdown]")
+TEST_CASE("add node to system", "[sec21][core]")
 {
    using namespace sec21;
 
    const std::vector<int> input{ 15, 22, 22, 23, 21, 25, 28, 24, 23, 22, 12, 16, 20, 30, 35, 29, 25, 21, 22, 26, 28 };
-
-   /* 
-   The MDD problem can be formulated as follows: given an array A, find the maximum 
-   difference A[j] - A[i] with j < i. The constraint on i and j makes this problem challenging. 
-   Without it we can just find the maximum and minimum elements of the array.
-   */
 
    SECTION("example range 1")
    {
