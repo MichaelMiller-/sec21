80751722bf4d1342203b5eed46d8f05c4f6a577e
michael miller <miller.michael@gmx.de>
2020-04-25 12:33:01 +0200

    initial commit

---
+++ include/sec21/structural_analysis/type_traits.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+namespace sec21::structural_analysis
+{
+   struct space_truss;
+
+   template <typename T>
+   struct is_space_truss : std::is_same<T, space_truss> {};
+
+   template <typename T>
+   struct is_space_truss_2D : std::conjunction<is_space_truss<T>, std::bool_constant<T::dimension_v == 2>> {};
+
+#ifdef __cpp_concepts
+   template <typename T>
+   concept SpaceTruss = is_space_truss<T>::value;
+
+   template <typename T>
+   concept SpaceTruss2D = SpaceTruss<T> && is_space_truss_2D<T>::value;
+#endif
+}
