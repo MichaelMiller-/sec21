142e2520a176b5fbf6cd60f8af94db3e7fde035d
michael miller <miller.michael@gmx.de>
2020-04-26 08:04:12 +0200

    add customization point for usertypes

--- include/sec21/access.h
+++ include/sec21/access.h
@@ -1,26 +1,76 @@
 #pragma once
 
+#include <array>
+
 namespace sec21
 {
-   //! \todo customization point -> boost::geometry, ..
-   //! \todo write access -> auto& 
-
-   template <typename T> 
-   constexpr auto X(T&& t) noexcept
+   namespace customization_point
    {
-      return std::get<0>(std::forward<T>(t));
+      template <typename T>
+      struct access_x;
+
+      template <typename T>
+      struct access_y;
+
+      template <typename T>
+      struct access_z;      
+
+      template <typename T, auto N>
+      struct access_x<std::array<T, N>>
+      {
+         auto operator()(std::array<T, N> const& t) const noexcept
+         {
+            return std::get<0>(t);
+         }
+         auto& operator()(std::array<T, N>& t) const noexcept
+         {
+            return std::get<0>(t);
+         }        
+      };
+
+      template <typename T, auto N>
+      struct access_y<std::array<T, N>>
+      {
+         auto operator()(std::array<T, N> const& t) const noexcept
+         {
+            return std::get<1>(t);
+         }
+         auto& operator()(std::array<T, N>& t) const noexcept
+         {
+            return std::get<1>(t);
+         }        
+      };
+
+      template <typename T, auto N>
+      struct access_z<std::array<T, N>>
+      {
+         auto operator()(std::array<T, N> const& t) const noexcept
+         {
+            return std::get<2>(t);
+         }
+         auto& operator()(std::array<T, N>& t) const noexcept
+         {
+            return std::get<2>(t);
+         }        
+      };
    }
 
    template <typename T> 
-   constexpr auto Y(T&& t) noexcept
+   constexpr decltype(auto) X(T&& t) noexcept
    {
-      return std::get<1>(std::forward<T>(t));
+      return customization_point::access_x<std::decay_t<T>>()(std::forward<T>(t));
    }
 
    template <typename T> 
-   constexpr auto Z(T&& t) noexcept
+   constexpr decltype(auto) Y(T&& t) noexcept
    {
-      return std::get<2>(std::forward<T>(t));
+      return customization_point::access_y<std::decay_t<T>>()(std::forward<T>(t));
+   }
+
+   template <typename T> 
+   constexpr decltype(auto) Z(T&& t) noexcept
+   {
+      return customization_point::access_z<std::decay_t<T>>()(std::forward<T>(t));
    }
 
 } // namespace sec21
