781bf74fe15ddbe4fc661e9acef918cdbdbd06dd
michael miller <miller.michael@gmx.de>
2020-04-26 08:14:16 +0200

    use access abstraction and customization point

--- include/sec21/structural_analysis/impl/geometry.h
+++ include/sec21/structural_analysis/impl/geometry.h
@@ -1,38 +1,68 @@
 #pragma once
 
-#include <boost/math/constants/constants.hpp>
+#include <sec21/access.h>
+
+#ifdef __cpp_lib_math_constants
+   #include <numbers>
+   #warning TODO: replace with std::numbers::
+#else 
+   #include <boost/math/constants/constants.hpp>
+#endif
+
+namespace sec21::customization_point
+{
+   template <typename T, auto N, typename CoordinateSystem>
+   struct access_x<boost::geometry::model::point<T, N, CoordinateSystem>>
+   {
+      auto operator()(boost::geometry::model::point<T, N, CoordinateSystem> const& t) const noexcept
+      {
+         return boost::geometry::get<0>(t);
+      }
+      auto& operator()(boost::geometry::model::point<T, N, CoordinateSystem>& t) const noexcept
+      {
+         return boost::geometry::get<0>(t);
+      }        
+   };
+
+   template <typename T, auto N, typename CoordinateSystem>
+   struct access_y<boost::geometry::model::point<T, N, CoordinateSystem>>
+   {
+      auto operator()(boost::geometry::model::point<T, N, CoordinateSystem> const& t) const noexcept
+      {
+         return boost::geometry::get<1>(t);
+      }
+      auto& operator()(boost::geometry::model::point<T, N, CoordinateSystem>& t) const noexcept
+      {
+         return boost::geometry::get<1>(t);
+      }        
+   };   
+}
 
 namespace sec21::structural_analysis::impl
 {
-   //! \todo 2019-04-17 error handling -> outcome
+   //! \todo  angle_to(...., Axis == )
+
    template <typename T>
-// #ifdef __cpp_concepts
-//    requires Node<T>
-// #endif      
-   auto angle_to_x_axis(T const& start, T const& end) noexcept
+   auto angle_to_x_axis(T const& start_position, T const& end_position) noexcept -> double
    {
-      auto pos_start = start.position;
-      auto pos_end = end.position;
-
-      //! \todo customzation_point for boost::geometry::point
-      namespace bg = boost::geometry;
-
-      auto dx = (bg::get<0>(pos_end) - bg::get<0>(pos_start));
-      auto dy = (bg::get<1>(pos_end) - bg::get<1>(pos_start));
-      auto m = dy / dx; // steigung der geraden
+      const auto dx = X(end_position) - X(start_position);
+      const auto dy = Y(end_position) - Y(start_position);
+      const auto m = dy / dx; // steigung der geraden
 
       if (std::abs(m) == 1)
-         return boost::math::constants::half_pi<typename T::precision_t>() * 0.5 * m;
+         return boost::math::constants::half_pi<double>() * 0.5 * m;
       return std::atan(m / 1 + m);
    }
 
-   //! \todo 2019-04-18 seams that only the unittests need this function
    //! \todo 2019-04-17 error handling -> outcome
    template <typename System>
-   auto angle_to_x_axis(System const& sys, typename System::member_descriptor_t id)
+   auto angle_to_x_axis(System const& sys, typename System::member_descriptor_t id) // -> outcome::std_result<double>
    {
       //! \todo can throw
-      const auto[s, e] = sys.adjacency_list.at(id);
-      return angle_to_x_axis(sys.nodes.at(s), sys.nodes.at(e));
+      const auto[s, e] = sys.coincidence_table.at(id);
+      const auto from = get_element(sys.nodes, s);
+      const auto to = get_element(sys.nodes, e);
+
+      return angle_to_x_axis(from->position, to->position);
    };
 }
