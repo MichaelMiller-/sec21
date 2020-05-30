f17843d783f9c422bef82b436cf6bfdcce98f0cc
michael miller <miller.michael@gmx.de>
2020-05-30 12:47:52 +0200

    add literal overload

--- include/sec21/units/dimensions/temperature.h
+++ include/sec21/units/dimensions/temperature.h
@@ -8,10 +8,12 @@ namespace sec21::units
    using temperature = dimension<exponent<base_dimension_temperature, 1>>;
 
    struct kelvin : coherent_derived_unit<kelvin, temperature, si_tag> {};
+   //! \todo celsius : coherent_derived_unit<celsius, temperature, si_tag> {};
 
    inline namespace literals
    {
       constexpr auto operator "" _K(unsigned long long v) noexcept  { return quantity<kelvin, unsigned long long>{ v }; }
+      constexpr auto operator "" _K(long double v) noexcept         { return quantity<kelvin, long double>{ v }; }
    }
 
 #ifdef __cpp_concepts
