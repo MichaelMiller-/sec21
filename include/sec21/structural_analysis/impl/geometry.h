7d52d6634b85039294f5aa897ae178af9969f935
michael miller <miller.michael@gmx.de>
2020-06-05 10:23:53 +0200

    move length() into impl-namespace

--- include/sec21/structural_analysis/impl/geometry.h
+++ include/sec21/structural_analysis/impl/geometry.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <sec21/access.h>
+#include <sec21/structural_analysis/node.h>
+// #include <sec21/structural_analysis/error_codes.h>
+#include <sec21/units/dimensions/length.h>
 
 #ifdef __cpp_lib_math_constants
    #include <numbers>
@@ -9,65 +11,11 @@
    #include <boost/math/constants/constants.hpp>
 #endif
 
-#include <boost/geometry/geometries/point.hpp>
-#include <boost/qvm/all.hpp>
-#include <boost/qvm/quat_traits_array.hpp>
+#include <boost/qvm/vec_traits_defaults.hpp>
+#include <boost/qvm/vec_operations.hpp>
+#include <boost/qvm/vec_access.hpp>
 
-namespace sec21::customization_point
-{
-   template <typename T, auto N, typename CoordinateSystem>
-   struct access_x<boost::geometry::model::point<T, N, CoordinateSystem>>
-   {
-      auto operator()(boost::geometry::model::point<T, N, CoordinateSystem> const& t) const noexcept
-      {
-         return boost::geometry::get<0>(t);
-      }
-      auto& operator()(boost::geometry::model::point<T, N, CoordinateSystem>&& t) const noexcept
-      {
-         return boost::geometry::get<0>(t);
-      }        
-   };
-
-   template <typename T, auto N, typename CoordinateSystem>
-   struct access_y<boost::geometry::model::point<T, N, CoordinateSystem>>
-   {
-      auto operator()(boost::geometry::model::point<T, N, CoordinateSystem> const& t) const noexcept
-      {
-         return boost::geometry::get<1>(t);
-      }
-      auto& operator()(boost::geometry::model::point<T, N, CoordinateSystem>&& t) const noexcept
-      {
-         return boost::geometry::get<1>(t);
-      }        
-   };   
-}
-
-#if 0 //! \todo not working
-template <typename T, auto N, typename CoordinateSystem>
-using point_t = boost::geometry::model::point<T, N, CoordinateSystem>;
-
-namespace boost::qvm 
-{
-   template <typename T, auto N, typename CoordinateSystem>
-   struct vec_traits<point_t<T, N, CoordinateSystem>> : vec_traits_defaults<point_t<T, N, CoordinateSystem>, T, N> 
-   {
-      using scalar_type = T;
-
-      template <int I>
-      static inline scalar_type& write_element(point_t<T, N, CoordinateSystem>& v) 
-      {
-         return boost::geometry::get<I>(v);
-         // static double r{};
-         // return r;
-      }
-
-      // static inline scalar_type & write_element_idx(int i, point_t& v) 
-      // {
-      //    return v.a[i];
-      // } //optional
-   };
-}
-#endif
+#include <stdexcept>
 
 namespace boost::qvm 
 {
@@ -76,7 +24,7 @@ namespace boost::qvm
    {
       using scalar_type = T;
 
-      template <int I>
+      template <auto I>
       static inline scalar_type& write_element(std::array<T, N>& v) 
       {
          return std::get<I>(v);
@@ -86,6 +34,42 @@ namespace boost::qvm
 
 namespace sec21::structural_analysis::impl
 {
+   template <auto Dimension, typename Precision>
+   [[nodiscard]] auto distance(node<Dimension, Precision> const& from, node<Dimension, Precision> const& to) -> Precision
+   {
+      using namespace boost::qvm;
+      return { mag(to.position - from.position) };
+   }
+
+   template <typename System>
+   [[nodiscard]] auto length(System const& sys, typename System::member_descriptor_t id) -> units::quantity<units::meter, typename System::precision_t>
+   {
+      const auto [s, e] = sys.coincidence_table.at(id);
+      
+      const auto from = std::find_if(
+         begin(sys.nodes), 
+         end(sys.nodes), 
+         [s](auto const& n){ return n.id == s; });
+
+      const auto to = std::find_if(
+         begin(sys.nodes), 
+         end(sys.nodes), 
+         [e](auto const& n){ return n.id == e; });
+
+      if (from == end(sys.nodes) || to == end(sys.nodes))
+         throw std::invalid_argument("invalid node reference");
+
+      return { distance(*from, *to) };
+   }   
+
+   //! \todo 
+   // auto start_and_end_position() -> std::pair<>
+   // {
+   //    const auto[s, e] = sys.coincidence_table.at(id);
+   //    const auto from = get_element(sys.nodes, s);
+   //    const auto to = get_element(sys.nodes, e);      
+   // }
+
    //! \todo  angle_to(...., Axis == )
    // template <typename Rotation> // = CounterClockwise>
    // struct angle_t
@@ -93,15 +77,11 @@ namespace sec21::structural_analysis::impl
    //    auto degree() const { return 3; }
    // };
 
-   template <typename T>
-   auto angle_to_x_axis(T const& start_position, T const& end_position) noexcept -> double
+   template <typename Position>
+   [[nodiscard]] auto angle_to_x_axis(Position const& from, Position const& to) noexcept -> double
    {
-      using value_t = std::array<double, 2>;
-      //! \todo remove conversion
-      auto from = value_t{ X(start_position), Y(start_position) };
-      auto to = value_t{ X(end_position), Y(end_position) };
-
       using namespace boost::qvm;
+      using namespace boost::math;
 
       const auto dir = to - from;
       decltype(dir) project_x{ X(dir) };
@@ -111,10 +91,10 @@ namespace sec21::structural_analysis::impl
 
       //! \todo approx
       if (m == 1)
-         return boost::math::constants::half_pi<double>() * 0.5;
+         return constants::half_pi<double>() * 0.5;
 
       if (0 == (l1 * l2))
-         return boost::math::constants::half_pi<double>();
+         return constants::half_pi<double>();
 
       auto k1 = 1;
       if (Y(dir) < 0)
@@ -122,19 +102,33 @@ namespace sec21::structural_analysis::impl
       if (X(dir) < 0)
          k1 *= -1;
 
-      //! \todo odd accos function?
       return std::acos(m / (l1 * l2)) * k1;
    }
 
-   //! \todo 2019-04-17 error handling -> outcome
-   template <typename System>
-   auto angle_to_x_axis(System const& sys, typename System::member_descriptor_t id) // -> outcome::std_result<double>
+   template <auto Dimension, typename Precision>
+   [[nodiscard]] auto angle_to_x_axis(node<Dimension, Precision> const& from, node<Dimension, Precision> const& to)
    {
-      //! \todo can throw
-      const auto[s, e] = sys.coincidence_table.at(id);
-      const auto from = get_element(sys.nodes, s);
-      const auto to = get_element(sys.nodes, e);
+      return angle_to_x_axis(from.position, to.position);
+   }
 
-      return angle_to_x_axis(from->position, to->position);
+   template <typename System>
+   [[nodiscard]] auto angle_to_x_axis(System const& sys, typename System::member_descriptor_t id)
+   {
+      const auto [s, e] = sys.coincidence_table.at(id);
+
+      const auto from = std::find_if(
+         begin(sys.nodes), 
+         end(sys.nodes), 
+         [s](auto const& n){ return n.id == s; });
+
+      const auto to = std::find_if(
+         begin(sys.nodes), 
+         end(sys.nodes), 
+         [e](auto const& n){ return n.id == e; });
+
+      if (from == end(sys.nodes) || to == end(sys.nodes))
+         throw std::invalid_argument("invalid node reference");
+
+      return angle_to_x_axis(*from, *to);
    };
 }
