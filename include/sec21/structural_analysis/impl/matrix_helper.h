238a556985e91d3997388f748d128a90c30915d1
michael miller <miller.michael@gmx.de>
2020-06-11 13:07:25 +0200

    new function remove_from_vector()

--- include/sec21/structural_analysis/impl/matrix_helper.h
+++ include/sec21/structural_analysis/impl/matrix_helper.h
@@ -8,17 +8,7 @@
 
 namespace sec21::structural_analysis::impl
 {
-   template <typename T = double, typename Iterator>
-   // requires T == std::iterator_traits<Iterator>::value_type
-   [[nodiscard]] auto make_vector(Iterator first, Iterator last) -> boost::numeric::ublas::vector<T>
-   {
-      using value_t = typename std::iterator_traits<Iterator>::value_type;
-      const auto n = std::distance(first, last);
-      boost::numeric::ublas::unbounded_array<T> storage(n);
-      std::copy(first, last, storage.begin());
-      return boost::numeric::ublas::vector<T>(n, storage);
-   }     
-
+   
    template <typename Iterator, typename F = boost::numeric::ublas::row_major>
    [[nodiscard]] auto make_matrix(std::size_t m, std::size_t n, Iterator first, Iterator last)
       -> boost::numeric::ublas::matrix<typename std::iterator_traits<Iterator>::value_type, F> 
@@ -33,7 +23,8 @@ namespace sec21::structural_analysis::impl
    }
    
    template <typename T>
-   auto remove_from_matrix(boost::numeric::ublas::matrix<T> in, std::vector<Row> const &rows, std::vector<Col> const &cols)
+   //! \todo write test
+   auto remove_from_matrix(boost::numeric::ublas::matrix<T> in, std::vector<Row> const& rows, std::vector<Col> const& cols)
    {
       using namespace boost::numeric;
       ublas::matrix<T> result(in.size1() - std::size(rows), in.size2() - std::size(cols));
@@ -56,6 +47,27 @@ namespace sec21::structural_analysis::impl
          ++rc;
       }
       return result;
-   }   
+   }
+
+   template <typename T>
+   //! \todo write test
+   auto remove_from_vector(boost::numeric::ublas::vector<T> in, std::vector<Row> const& rows)
+   {
+      using namespace boost::numeric;
+      ublas::vector<T> result(in.size() - std::size(rows));
+      auto it = begin(result);
+
+      for (decltype(in.size()) i = 0; i < in.size(); ++i)
+      {
+         if (::sec21::contains(rows, Row{i}))
+            continue;
+
+         *it = in[i];
+         ++it;
+      }      
+      return result;
+   }
+
+
 } // namespace sec21::structural_analysis
 
