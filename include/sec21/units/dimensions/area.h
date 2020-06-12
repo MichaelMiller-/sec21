13d9921e9e11bd4d064549a02f4020225e69d745
michael miller <miller.michael@gmx.de>
2020-06-12 21:05:54 +0200

    add abbreviation

--- include/sec21/units/dimensions/area.h
+++ include/sec21/units/dimensions/area.h
@@ -3,6 +3,8 @@
 #include <sec21/units/dimensions/base_dimensions.h>
 #include <sec21/units/quantity.h>
 
+#include <string_view>
+
 namespace sec21::units
 {
    using area = dimension<exponent<base_dimension_length, 2>>;
@@ -11,6 +13,38 @@ namespace sec21::units
    struct square_millimeter : derived_unit<square_millimeter, area, std::micro> {};
    struct square_centimeter : derived_unit<square_centimeter, area, std::ratio<1, 10000>> {};
    struct square_kilometer : derived_unit<square_kilometer, area, std::mega> {};
+
+   template <>
+   struct abbreviation<square_meter>
+   {
+      using type_t = square_meter;
+      constexpr static std::string_view value{"sq_m"};
+   };
+   template <>
+   struct abbreviation<square_millimeter>
+   {
+      using type_t = square_millimeter;
+      constexpr static std::string_view value{"sq_mm"};
+   };
+   template <>
+   struct abbreviation<square_centimeter>
+   {
+      using type_t = square_centimeter;
+      constexpr static std::string_view value{"sq_cm"};
+   };
+   template <>
+   struct abbreviation<square_kilometer>
+   {
+      using type_t = square_kilometer;
+      constexpr static std::string_view value{"sq_km"};
+   };
+
+   template <>
+   struct type_info<area>
+   {
+      using valid_types_t = std::tuple<square_meter, square_millimeter, square_centimeter, square_kilometer>;
+      constexpr static std::string_view name{"area"};
+   };
 
    inline namespace literals
    {
