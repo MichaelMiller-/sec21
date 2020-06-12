339672dd7c1ff0ced73c45fb3a4f3677df973a8e
michael miller <miller.michael@gmx.de>
2020-06-12 08:59:08 +0200

    move content to concepts header

--- include/sec21/structural_analysis/type_traits.h
+++ include/sec21/structural_analysis/type_traits.h
@@ -2,19 +2,5 @@
 
 namespace sec21::structural_analysis
 {
-   struct space_truss;
-
-   template <typename T>
-   struct is_space_truss : std::is_same<T, space_truss> {};
-
-   template <typename T>
-   struct is_space_truss_2D : std::conjunction<is_space_truss<T>, std::bool_constant<T::dimension_v == 2>> {};
-
-#ifdef __cpp_concepts
-   template <typename T>
-   concept SpaceTruss = is_space_truss<T>::value;
-
-   template <typename T>
-   concept SpaceTruss2D = SpaceTruss<T> && is_space_truss_2D<T>::value;
-#endif
+//! \todo remove
 }
