0cc3159a218a3cdd8f2603ce0fa9e50f0fabb4a6
michael miller <miller.michael@gmx.de>
2020-06-12 21:06:56 +0200

    add abbreviation

--- include/sec21/units/dimensions/pressure.h
+++ include/sec21/units/dimensions/pressure.h
@@ -17,9 +17,44 @@ namespace sec21::units
    // clang-format on
 
    struct pascal : coherent_derived_unit<pascal, pressure> {};
+   struct kilopascal : derived_unit<kilopascal, pressure, std::kilo> {};
+   struct megapascal : derived_unit<megapascal, pressure, std::mega> {};
+
+   template <>
+   struct abbreviation<pascal>
+   {
+      using type_t = pascal;
+      constexpr static std::string_view value{"Pa"};
+   };
+   template <>
+   struct abbreviation<kilopascal>
+   {
+      using type_t = kilopascal;
+      constexpr static std::string_view value{"kPa"};
+   };
+   template <>
+   struct abbreviation<megapascal>
+   {
+      using type_t = megapascal;
+      constexpr static std::string_view value{"MPa"};
+   };
+
+   template <>
+   struct type_info<pressure>
+   {
+      using valid_types_t = std::tuple<pascal, kilopascal, megapascal>;
+      constexpr static std::string_view name{"pressure"};
+   };
 
    inline namespace literals
    {
       constexpr auto operator "" _Pa(unsigned long long v) noexcept  { return quantity<pascal, unsigned long long>{ v }; }
+      constexpr auto operator "" _Pa(long double v) noexcept         { return quantity<pascal, long double>{ v }; }
+
+      constexpr auto operator "" _kPa(unsigned long long v) noexcept  { return quantity<kilopascal, unsigned long long>{ v }; }
+      constexpr auto operator "" _kPa(long double v) noexcept         { return quantity<kilopascal, long double>{ v }; }
+
+      constexpr auto operator "" _MPa(unsigned long long v) noexcept  { return quantity<megapascal, unsigned long long>{ v }; }
+      constexpr auto operator "" _MPa(long double v) noexcept         { return quantity<megapascal, long double>{ v }; }
    }
 }
