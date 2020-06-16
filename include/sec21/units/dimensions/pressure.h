66c5917a0a328b9b0bea485d9d20ce1961102bab
michael miller <miller.michael@gmx.de>
2020-06-16 15:50:24 +0200

    new stress units

--- include/sec21/units/dimensions/pressure.h
+++ include/sec21/units/dimensions/pressure.h
@@ -16,9 +16,15 @@ namespace sec21::units
       exponent<base_dimension_time, -2>>;
    // clang-format on
 
-   struct pascal : coherent_derived_unit<pascal, pressure> {};
+   struct pascal : derived_unit<pascal, pressure, base_unit> {};
    struct kilopascal : derived_unit<kilopascal, pressure, std::kilo> {};
    struct megapascal : derived_unit<megapascal, pressure, std::mega> {};
+
+   // stress
+   struct newton_per_square_meter : derived_unit<pascal, pressure, base_unit> {};
+   struct kilonewton_per_square_meter : derived_unit<newton_per_square_meter, pressure, std::kilo> {};
+
+   // struct psi : derived_unit<kilopascal, pressure, std::ratio<6.894757>> {};
 
    template <>
    struct abbreviation<pascal>
@@ -38,11 +44,24 @@ namespace sec21::units
       using type_t = megapascal;
       constexpr static std::string_view value{"MPa"};
    };
+   // stress
+   template <>
+   struct abbreviation<newton_per_square_meter>
+   {
+      using type_t = newton_per_square_meter;
+      constexpr static std::string_view value{"N/sq_m"};
+   };   
+   template <>
+   struct abbreviation<kilonewton_per_square_meter>
+   {
+      using type_t = kilonewton_per_square_meter;
+      constexpr static std::string_view value{"kN/sq_m"};
+   };
 
    template <>
    struct type_info<pressure>
    {
-      using valid_types_t = std::tuple<pascal, kilopascal, megapascal>;
+      using valid_types_t = std::tuple<pascal, kilopascal, megapascal, newton_per_square_meter, kilonewton_per_square_meter>;
       constexpr static std::string_view name{"pressure"};
    };
 
@@ -56,5 +75,12 @@ namespace sec21::units
 
       constexpr auto operator "" _MPa(unsigned long long v) noexcept  { return quantity<megapascal, unsigned long long>{ v }; }
       constexpr auto operator "" _MPa(long double v) noexcept         { return quantity<megapascal, long double>{ v }; }
+
+      // stress
+      constexpr auto operator "" _N_per_sqm(unsigned long long v) noexcept  { return quantity<newton_per_square_meter, unsigned long long>{ v }; }
+      constexpr auto operator "" _N_per_sqm(long double v) noexcept         { return quantity<newton_per_square_meter, long double>{ v }; }
+
+      constexpr auto operator "" _kN_per_sqm(unsigned long long v) noexcept  { return quantity<kilonewton_per_square_meter, unsigned long long>{ v }; }
+      constexpr auto operator "" _kN_per_sqm(long double v) noexcept         { return quantity<kilonewton_per_square_meter, long double>{ v }; }      
    }
 }
