2ed37ce48b471028325e780332808c2e5c2f7be5
michael miller <miller.michael@gmx.de>
2020-06-16 15:51:07 +0200

    literals overload for long double

--- include/sec21/units/dimensions/area.h
+++ include/sec21/units/dimensions/area.h
@@ -48,12 +48,17 @@ namespace sec21::units
 
    inline namespace literals
    {
-      constexpr auto operator "" _sq_m(unsigned long long v) noexcept  { return quantity<square_meter, unsigned long long>{ v }; }
+      constexpr auto operator "" _sq_m(unsigned long long v) noexcept   { return quantity<square_meter, unsigned long long>{ v }; }
+      constexpr auto operator "" _sq_m(long double v) noexcept         { return quantity<square_meter, long double>{ v }; }
+
       constexpr auto operator "" _sq_mm(unsigned long long v) noexcept  { return quantity<square_millimeter, unsigned long long>{ v }; }
+      constexpr auto operator "" _sq_mm(long double v) noexcept         { return quantity<square_millimeter, long double>{ v }; }
+
       constexpr auto operator "" _sq_cm(unsigned long long v) noexcept  { return quantity<square_centimeter, unsigned long long>{ v }; }
+      constexpr auto operator "" _sq_cm(long double v) noexcept         { return quantity<square_centimeter, long double>{ v }; }
 
       constexpr auto operator "" _sq_km(unsigned long long v) noexcept  { return quantity<square_kilometer, unsigned long long>{ v }; }
-      constexpr auto operator "" _sq_km(long double v) noexcept  { return quantity<square_kilometer, long double>{ v }; }
+      constexpr auto operator "" _sq_km(long double v) noexcept         { return quantity<square_kilometer, long double>{ v }; }
 
    }
 }
