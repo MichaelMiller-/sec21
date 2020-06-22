cf8cd110dff84344c59c03042b387cbca9a209af
michael miller <miller.michael@gmx.de>
2020-06-22 14:31:48 +0200

    initial commit

---
+++ include/sec21/structural_analysis/descriptor_traits.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <limits>
+
+namespace sec21::structural_analysis
+{
+   template <typename T>
+   struct descriptor_traits;
+
+   // default traits <int, std::string>
+   //
+   template <>
+   struct descriptor_traits<int>
+   {
+      using type_t = int;
+
+      static constexpr auto invalid() noexcept -> type_t
+      {
+         return std::numeric_limits<type_t>::max();
+      }
+   };
+
+   template <>
+   struct descriptor_traits<std::string>
+   {
+      using type_t = std::string;
+
+      static type_t invalid() noexcept
+      {
+         return {""};
+      }
+   };
+}
