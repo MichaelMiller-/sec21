d4bce20b3e8e8db85df1b0503d66f27412605db1
michael miller <miller.michael@gmx.de>
2020-07-27 08:03:40 +0200

    consider constexpr

--- tests/make_iota_array.cpp
+++ tests/make_iota_array.cpp
@@ -1,11 +1,23 @@
 #define CATCH_CONFIG_MAIN
-#include "catch.hpp"
+#include <catch.hpp>
 
 #include <sec21/make_iota_array.h>
 
 #include <algorithm>
 
-TEST_CASE("make iota array", "[core]")
+#ifdef __cpp_lib_constexpr_misc
+
+TEST_CASE("make iota array", "[sec21][core]") 
+{
+   constexpr auto result = make_iota_array<int, 4>();
+   const decltype(result) expected{ 0,1,2,3 };
+
+   STATIC_REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)), "sould be equal");
+}
+
+#else
+
+TEST_CASE("make iota array", "[sec21][core]")
 {
    using namespace sec21;
 
@@ -17,8 +29,9 @@ TEST_CASE("make iota array", "[core]")
    }
    SECTION("with offset")
    {
-      auto result = make_iota_array<int, 4>(2);
+      constexpr auto result = make_iota_array<int, 4>(2);
       const decltype(result) expected{ 2,3,4,5 };
       REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)) == true);
    }
 }   
+#endif
