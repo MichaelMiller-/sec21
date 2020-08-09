ee383ff288767c593baeba4ad4ebf2cb64bab5dd
michael miller <miller.michael@gmx.de>
2020-08-09 10:21:44 +0200

    add overload for std::tuple

--- include/sec21/enclose.h
+++ include/sec21/enclose.h
@@ -5,6 +5,7 @@
 #include <iterator>
 #include <algorithm>
 #include <sstream>
+#include <tuple>
 
 namespace sec21
 {
@@ -39,4 +40,42 @@ namespace sec21
       enclose(ss, std::forward<Sequence>(seq), delimiter, start, end);
       return ss.str();
    }
+
+   namespace detail
+   {
+      template <typename... Args, typename Function, auto... Idx>
+      void for_each(std::tuple<Args...> const& t, Function&& func, std::index_sequence<Idx...>)
+      {
+         (func(std::get<Idx>(t)), ...);
+      }
+   }
+
+   template <typename... Args, typename TransformFunction>
+   auto enclose(
+      std::tuple<Args...> args,
+      TransformFunction&& func,
+      std::string_view delimiter = ",",
+      std::string_view start = "(",
+      std::string_view end = ")")
+   {
+      constexpr auto N = std::tuple_size_v<decltype(args)>;
+      static_assert(N > 0, "cannot enclose a empty tuple");
+
+      std::stringstream ss{};
+      ss << start;
+      detail::for_each(args, [&](auto&& v) { ss << func(v) << delimiter; }, std::make_index_sequence<N-1>{});
+      ss << func(std::get<N-1>(args));
+      ss << end;
+      return ss.str();
+   }
+
+   template <typename... Args>
+   auto enclose(
+      std::tuple<Args...> args,
+      std::string_view delimiter = ",",
+      std::string_view start = "(",
+      std::string_view end = ")")
+   {
+      return enclose(args, [](auto v){ return v; }, delimiter, start, end);
+   }   
 } // sec21
--- tests/enclose.cpp
+++ tests/enclose.cpp
@@ -7,31 +7,27 @@
 TEST_CASE("string format for a example sequence", "[sec21][core]")
 {
    const std::vector<int> input{ 1, 2, 3 };
-
    auto result = sec21::enclose(input);
-
    const decltype(result) expected{"{1, 2, 3}"};
-   REQUIRE(std::size(result) == std::size(expected));
-   REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)));
+   REQUIRE(result == expected);
 }
 TEST_CASE("string format for a sequence with only one element", "[sec21][core]")
 {
    const std::vector<int> input{ 1 };
-
    auto result = sec21::enclose(input);
-
    const decltype(result) expected{"{1}"};
-   REQUIRE(std::size(result) == std::size(expected));
-   REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)));
+   REQUIRE(result == expected);
 }
 TEST_CASE("string format for a empty sequence", "[sec21][core]")
 {
    const std::vector<int> input{};
-
    auto result = sec21::enclose(input);
-
    const decltype(result) expected{"{}"};
-   REQUIRE(std::size(result) == std::size(expected));
-   REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)));
+   REQUIRE(result == expected);
 }
-
+TEST_CASE("enclose a std::tuple", "[sec21][core]")
+{
+   auto result = sec21::enclose(std::make_tuple("foo", 42, "bar"));
+   const decltype(result) expected{"(foo,42,bar)"};
+   REQUIRE(result == expected);
+}
