4805eed277e25ea6de04bc54dda92f7a41c48d10
michael miller <miller.michael@gmx.de>
2021-01-28 18:06:01 +0100

    deprecate function

--- include/sec21/contains.h
+++ include/sec21/contains.h
@@ -5,6 +5,7 @@
 namespace sec21
 {
    template <typename T>
+   [[deprecated("replace with std::any_of")]] 
    constexpr bool contains(T const& t, typename T::value_type const& v) noexcept
    {
       return std::find(std::begin(t), std::end(t), v) != std::end(t);
