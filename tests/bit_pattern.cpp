753f919780be87ec412c6d83879c1ac13f7b8ae3
michael miller <miller.michael@gmx.de>
2021-01-29 08:25:33 +0100

    c++17 compatibility

--- tests/bit_pattern.cpp
+++ tests/bit_pattern.cpp
@@ -16,7 +16,11 @@ namespace sec21
 #ifdef __cpp_concepts
       requires (Size > 2)
 #endif
+#ifdef __cpp_consteval
       explicit consteval bit_pattern(const char (&input)[Size])
+#else
+      explicit bit_pattern(const char (&input)[Size])
+#endif
       {
          static_assert(Size > 2);
 
@@ -41,7 +45,11 @@ namespace sec21
          }
       }
 
+#ifdef __cpp_concepts
       template <std::unsigned_integral T>
+#else
+      template <typename T>
+#endif      
       constexpr friend bool operator == (T const& value , bit_pattern const& pattern)
       {
          return (value & pattern.mask) == pattern.expected;
@@ -53,10 +61,19 @@ TEST_CASE("match bits via a pattern", "[sec21][core]")
 {
    using namespace sec21;
 
+#ifdef __cpp_consteval
    STATIC_REQUIRE(0b1111111u == bit_pattern("1111111"));
    STATIC_REQUIRE(0b1101010u == bit_pattern("11xxx10"));
    STATIC_REQUIRE(0b1111111u == bit_pattern("1111111"));
    STATIC_REQUIRE(0b0000000u == bit_pattern("0000000"));
    STATIC_REQUIRE(0b1101010u == bit_pattern("xxxxxxx"));
    STATIC_REQUIRE(0b1101010u == bit_pattern("xxx10xx"));
+#else
+   REQUIRE(0b1111111u == bit_pattern("1111111"));
+   REQUIRE(0b1101010u == bit_pattern("11xxx10"));
+   REQUIRE(0b1111111u == bit_pattern("1111111"));
+   REQUIRE(0b0000000u == bit_pattern("0000000"));
+   REQUIRE(0b1101010u == bit_pattern("xxxxxxx"));
+   REQUIRE(0b1101010u == bit_pattern("xxx10xx"));
+#endif
 }
