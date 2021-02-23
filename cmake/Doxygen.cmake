830700475fd3d39e3d74126148f1fc5d731ea1af
michael miller <miller.michael@gmx.de>
2021-02-23 15:38:19 +0100

    specify api-docs build target

--- cmake/Doxygen.cmake
+++ cmake/Doxygen.cmake
@@ -2,12 +2,25 @@ function(enable_doxygen)
   option(ENABLE_DOXYGEN "Enable doxygen doc builds of source" OFF)
 
   if(ENABLE_DOXYGEN)
-    set(DOXYGEN_CALLER_GRAPH ON)
-    set(DOXYGEN_CALL_GRAPH ON)
-    set(DOXYGEN_EXTRACT_ALL ON)
-    find_package(Doxygen REQUIRED dot)
+    find_package(Doxygen)
 
-    ## TODO: generic; pass folders to function
-    doxygen_add_docs(doxygen-docs ${PROJECT_SOURCE_DIR})
+    if (Doxygen_FOUND)
+      set(DOXYGEN_EXCLUDE_PATTERNS
+            */node_modules/*
+            */viennacl/*
+            */_build/*
+      )  
+      set(DOXYGEN_CALLER_GRAPH ON)
+      set(DOXYGEN_CALL_GRAPH ON)
+      set(DOXYGEN_EXTRACT_ALL ON)
+      set(DOXYGEN_SOURCE_BROWSER YES)
+      set(DOXYGEN_EXTRACT_PRIVATE YES)
+      set(DOXYGEN_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/api-docs")    
+
+      # make api-docs
+      doxygen_add_docs(api-docs ${PROJECT_SOURCE_DIR} ALL)
+    else()
+      message(WARNING "doxygen not found")
+    endif()
   endif()
 endfunction()
