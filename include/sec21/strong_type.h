5a53ed4a7a89829412c45f09569ec3c4b3afe2d9
michael miller <miller.michael@gmx.de>
2021-02-23 17:49:40 +0100

    cleanup header; improve execption handling; remove trivial type restriction

--- include/sec21/strong_type.h
+++ include/sec21/strong_type.h
@@ -10,13 +10,11 @@ namespace sec21
    {
    public:
       using underlying_t = T;
-      static_assert(std::is_trivial_v<underlying_t>, "strong_type: T should be a trivial type");
    private:
       underlying_t value{};
    public:
-
       constexpr strong_type() = default;
-      constexpr explicit strong_type(T const& t) noexcept
+      constexpr explicit strong_type(T const& t) noexcept(std::is_nothrow_copy_constructible<T>::value)
          : value{ t }
       {}
       constexpr explicit strong_type(T&& t) noexcept(std::is_nothrow_move_constructible<T>::value)
@@ -54,8 +52,6 @@ namespace sec21
    concept StrongType = is_strong_type_v<T>;
 #endif
 
-   //! \todo test if this is a good idea
-#if 0
    namespace detail
    {
       template <typename T, typename Tag, template<typename> typename... Policies>
@@ -64,157 +60,4 @@ namespace sec21
 
    template <typename T>
    using underlying_type = decltype(detail::underlying_type(std::declval<std::decay_t<T>>()));
-#endif
-
-   template <typename T, typename U>
-   struct mixed_addition
-   {
-      friend constexpr T& operator += (T& lhs, U const& rhs) noexcept
-      {
-         using lhs_t = typename T::underlying_t;
-         using rhs_t = typename U::underlying_t;
-         static_assert(std::is_same_v<lhs_t, rhs_t>);
-
-         static_cast<lhs_t&>(lhs) += static_cast<rhs_t const&>(rhs);
-         return lhs;
-      }
-      friend constexpr T operator + (T const& lhs, U const& rhs) noexcept
-      {
-         using lhs_t = typename T::underlying_t;
-         using rhs_t = typename U::underlying_t;
-         static_assert(std::is_same_v<lhs_t, rhs_t>);
-
-         return T(static_cast<lhs_t const&>(lhs) + static_cast<rhs_t const&>(rhs));
-      }
-   };
-
-   template <typename T>
-   using addition = mixed_addition<T, T>;
-
-
-   template <typename T, typename U>
-   struct mixed_subtraction
-   {
-      friend constexpr T& operator -= (T& lhs, U const& rhs) noexcept
-      {
-         using lhs_t = typename T::underlying_t;
-         using rhs_t = typename U::underlying_t;
-         static_assert(std::is_same_v<lhs_t, rhs_t>);
-
-         static_cast<lhs_t&>(lhs) -= static_cast<rhs_t const&>(rhs);
-         return lhs;
-      }
-      friend constexpr T operator - (T const& lhs, U const& rhs) noexcept
-      {
-         using lhs_t = typename T::underlying_t;
-         using rhs_t = typename U::underlying_t;
-         static_assert(std::is_same_v<lhs_t, rhs_t>);
-
-         return T(static_cast<lhs_t const&>(lhs) - static_cast<rhs_t const&>(rhs));
-      }
-   };
-
-   template <typename T>
-   using subtraction = mixed_subtraction<T, T>;
-
-
-   template <typename T>
-   struct multiplication
-   {
-      friend constexpr T& operator *= (T& lhs, T const& rhs) noexcept
-      {
-         using type = typename T::underlying_t;
-         static_cast<type&>(lhs) *= static_cast<type const&>(rhs);
-         return lhs;
-      }
-      friend constexpr T operator * (T const& lhs, T const& rhs) noexcept
-      {
-         using type = typename T::underlying_t;
-         return T(static_cast<type const&>(lhs) * static_cast<type const&>(rhs));
-      }
-   };
-
-   template <typename T>
-   struct division
-   {
-      friend constexpr T& operator /= (T& lhs, T const& rhs) noexcept
-      {
-         using type = typename T::underlying_t;
-         static_cast<type&>(lhs) /= static_cast<type const&>(rhs);
-         return lhs;
-      }
-      friend constexpr T operator / (T const& lhs, T const& rhs) noexcept
-      {
-         using type = typename T::underlying_t;
-         return T(static_cast<type const&>(lhs) / static_cast<type const&>(rhs));
-      }
-   };
-
-   template <typename T>
-   struct increment
-   {
-      constexpr T& operator ++ () noexcept
-      {
-         using type = typename T::underlying_t;
-         ++static_cast<type&>(static_cast<T&>(*this));
-         return static_cast<T&>(*this);
-      }
-      constexpr T operator ++ (int) noexcept
-      {
-         auto result = static_cast<T&>(*this);
-         ++*this;
-         return result;
-      }
-   };
-
-   template <typename T>
-   struct decrement
-   {
-      constexpr T& operator -- () noexcept
-      {
-         using type = typename T::underlying_t;
-         --static_cast<type&>(static_cast<T&>(*this));
-         return static_cast<T&>(*this);
-      }
-      constexpr T operator -- (int) noexcept
-      {
-         auto result = static_cast<T&>(*this);
-         --*this;
-         return result;
-      }
-   };
-
-   template <typename T>
-   struct integer_arithmetic : addition<T>,
-                               subtraction<T>,
-                               multiplication<T>,
-                               division<T>,
-                               increment<T>,
-                               decrement<T>
-   { };
-
-   template <typename T>
-   struct compare
-   {
-      friend constexpr bool operator == (T const& lhs, const T& rhs) noexcept
-      {
-         using type = typename T::underlying_t;
-         return static_cast<type const&>(lhs) == static_cast<type const&>(rhs);
-      }
-      friend constexpr bool operator != (T const& lhs, const T& rhs) noexcept
-      {
-         return !(lhs == rhs);
-      }
-   };
-
-   template <typename T>
-   struct print
-   {
-      template <typename CharT, typename Traits>
-      friend auto& operator << (std::basic_ostream<CharT, Traits>& os, T const& t) noexcept
-      {
-         using type = typename T::underlying_t;
-         return os << static_cast<type const&>(t);
-      }
-   };
 }
