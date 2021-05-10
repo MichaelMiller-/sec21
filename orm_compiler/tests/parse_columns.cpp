13d67e062fb35a0e50fd071f28c7baa646f27249
MichaelMiller <miller.michael@gmx.de>
2021-05-10 17:26:08 +0200

    initial commit

---
+++ orm_compiler/tests/parse_columns.cpp
@@ -0,0 +1,28 @@
+﻿#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+
+#include <string>
+using namespace std::literals;
+
+TEST_CASE("access a std::array", "[sec21][orm_compiler]")
+{
+   const auto input = R"(
+namespace sec21::structural_analysis
+{
+    [[Entity]]
+    struct structural_point
+    {
+        [[Column]]
+        std::string name  ;
+        
+        [[Column]][[SqlString]]
+        boost::uuids::uuid tag;
+        
+        [[Column]]
+        double coordinate_x;
+        double coordinate_y;
+        double coordinate_z;
+    };
+})"s;
+
+}
