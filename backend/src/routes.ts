5e605c747ff0712087daed1042fb55b376312641
michael miller <miller.michael@gmx.de>
2021-05-23 21:12:55 +0200

    add various endpoints

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,24 +1,28 @@
 import { listProjects } from "./controller/ListProjects";
-import { addProject } from "./controller/AddProject";
 import { listStructuralPoints } from "./controller/ListStructuralPoints"
-import { addStructuralPoint } from "./controller/AddStructuralPoint";
 import { listMaterials } from "./controller/ListMaterials";
 import { listCrossSections } from "./controller/ListCrossSections";
+import { listLoadCases } from "./controller/ListLoadCases";
+import { listCurveMembers } from "./controller/ListCurveMembers";
+import { addProject } from "./controller/AddProject";
+import { addStructuralPoint } from "./controller/AddStructuralPoint";
+import { addMaterial } from "./controller/AddMaterial";
+import { addCrossSection } from "./controller/AddCrossSection";
+import { addLoadCase } from "./controller/AddLoadCase";
+import { addPointAction } from "./controller/AddPointAction";
+import { addCurveMember } from "./controller/AddCurveMember";
+import { deleteStructuralPoint } from "./controller/DeleteStructuralPoint";
 import { getProject } from "./controller/GetProject";
 import { getMaterial } from "./controller/GetMaterial";
 import { getCrossSection } from "./controller/GetCrossSection";
-import { deleteStructuralPoint } from "./controller/DeleteStructuralPoint";
+import { addPointSupport } from "./controller/AddPointSupport";
+import { listPointSupports } from "./controller/ListPointSupports";
 
 export const AppRoutes = [
     {
         path: "/projects",
         method: "get",
         action: listProjects
-    },
-    {
-        path: "/projects",
-        method: "post",
-        action: addProject
     },
     {
         path: "/structuralpoints/:id",
@@ -36,12 +40,62 @@ export const AppRoutes = [
         action: listCrossSections
     },
     {
+        path: "/loadcases/:id",
+        method: "get",
+        action: listLoadCases
+    },
+    {
+        path: "/curvemembers/:id",
+        method: "get",
+        action: listCurveMembers
+    },
+    {
+        path: "/pointsupports/:id",
+        method: "get",
+        action: listPointSupports
+    },
+    {
+        path: "/projects",
+        method: "post",
+        action: addProject
+    },
+    {
         path: "/structuralpoints/:id",
         method: "post",
         action: addStructuralPoint
     },
     {
-        path: "/structuralpoint/:id",
+        path: "/materials/:id",
+        method: "post",
+        action: addMaterial
+    },
+    {
+        path: "/crosssections/:id",
+        method: "post",
+        action: addCrossSection
+    },
+    {
+        path: "/loadcases/:id",
+        method: "post",
+        action: addLoadCase
+    },
+    {
+        path: "/pointaction/:loadcase/:pt",
+        method: "post",
+        action: addPointAction
+    },
+    {
+        path: "/pointsupport/:pt",
+        method: "post",
+        action: addPointSupport
+    },
+    {
+        path: "/curvemember/:id/:from/:to",
+        method: "post",
+        action: addCurveMember
+    },
+    {
+        path: "/structuralpoint:id",
         method: "delete",
         action: deleteStructuralPoint
     },
