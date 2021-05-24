9372955722d359d279db0b70aadbafef3faa7729
michael miller <miller.michael@gmx.de>
2021-05-24 11:49:58 +0200

    add missing include

--- include/sec21/structural_analysis/loadcase.h
+++ include/sec21/structural_analysis/loadcase.h
@@ -1,13 +1,15 @@
 #pragma once
 
-#include <sec21/units.h>
 #include <sec21/structural_analysis/concepts.h>
 #include <sec21/structural_analysis/force.h>
 #include <sec21/structural_analysis/impl/geometry.h>
+#include <sec21/units.h>
 
+#include <boost/uuid/uuid.hpp>
+
+#include <algorithm>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 namespace sec21::structural_analysis
 {
@@ -46,13 +48,11 @@ namespace sec21::structural_analysis
 
    //! \todo remove template parameter LoadPair
    template <typename System, typename LoadPair, typename Iterator>
-   auto copy_load(System const &sys, LoadPair const &load, Iterator first)
+   auto copy_load(System const& sys, LoadPair const& load, Iterator first)
    {
       constexpr auto dim = System::dimension_v;
-      const auto it = std::find_if(
-          begin(sys.nodes),
-          end(sys.nodes),
-          [&load](auto const &m) { return m.name == load.first; });
+      const auto it =
+         std::find_if(begin(sys.nodes), end(sys.nodes), [&load](auto const& m) { return m.name == load.first; });
 
       if (it == end(sys.nodes))
          throw std::invalid_argument("invalid node id");
@@ -64,40 +64,31 @@ namespace sec21::structural_analysis
    }
 
    template <typename System, typename Unit, typename Allocator>
-   auto add_node_load(System const &sys, loadcase<System> const &load, std::vector<Unit, Allocator> &F)
+   auto add_node_load(System const& sys, loadcase<System> const& load, std::vector<Unit, Allocator>& F)
    {
       // constexpr auto dim = System::dimension_v;
       //! \todo std::for_each(std::execution::par
-      for (auto const &lf : load.node_load)
+      for (auto const& lf : load.node_load)
          copy_load(sys, lf, std::begin(F));
    }
 
    template <typename System, typename Iterator>
-   auto add_temperature_load(System const &sys, loadcase<System> const &load, Iterator first)
+   auto add_temperature_load(System const& sys, loadcase<System> const& load, Iterator first)
    {
       constexpr auto dim = System::dimension_v;
       //! \todo std::for_each(std::execution::par
-      for (auto const &lf : load.temperature_loads)
-      {
-         const auto it = std::find_if(
-             begin(sys.members),
-             end(sys.members),
-             [id = lf.member_id](auto const &m) { return m.name == id; });
+      for (auto const& lf : load.temperature_loads) {
+         const auto it = std::find_if(begin(sys.members), end(sys.members),
+                                      [id = lf.member_id](auto const& m) { return m.name == id; });
 
          if (it == std::end(sys.members))
             throw std::invalid_argument("member id not found");
 
          const auto [s, e] = sys.coincidence_table.at(lf.member_id);
 
-         const auto from = std::find_if(
-             begin(sys.nodes),
-             end(sys.nodes),
-             [s](auto const &n) { return n.name == s; });
+         const auto from = std::find_if(begin(sys.nodes), end(sys.nodes), [s](auto const& n) { return n.name == s; });
 
-         const auto to = std::find_if(
-             begin(sys.nodes),
-             end(sys.nodes),
-             [e](auto const &n) { return n.name == e; });
+         const auto to = std::find_if(begin(sys.nodes), end(sys.nodes), [e](auto const& n) { return n.name == e; });
 
          //! \todo error handling
          const auto alpha = impl::angle_to_x_axis(*from, *to);
@@ -122,8 +113,7 @@ namespace sec21::structural_analysis
       }
    }
 
-   enum class LoadType
-   {
+   enum class LoadType {
       SelfWeight,
       Others,
       Prestress,
@@ -139,98 +129,75 @@ namespace sec21::structural_analysis
       Standard
    };
 
-   enum class LoadDuration
-   {
-      Long,
-      Medium,
-      Short,
-      Instantaneous
-   };
+   enum class LoadDuration { Long, Medium, Short, Instantaneous };
 
-   enum class CoordinateSystem
-   {
-      Global,
-      Local
-   };
+   enum class CoordinateSystem { Global, Local };
 
-   enum class Direction
-   {
-      X = 0,
-      Y = 1,
-      Z = 2
-   };
+   enum class Direction { X = 0, Y = 1, Z = 2 };
 
-   enum class ActionType
-   {
-      Permanent,
-      Variable,
-      Accidental
-   };
+   enum class ActionType { Permanent, Variable, Accidental };
 
    struct point_action
    {
       std::string name;
       boost::uuids::uuid tag;
-      LoadType type{ LoadType::Standard };
-      ActionType action_type{ ActionType::Permanent };
-      Direction direction{ Direction::Z };
+      LoadType type{LoadType::Standard};
+      ActionType action_type{ActionType::Permanent};
+      Direction direction{Direction::Z};
       // reference_node
       double value; // [kN]
       // loadcase
-      CoordinateSystem coordinate_system{ CoordinateSystem::Global };
+      CoordinateSystem coordinate_system{CoordinateSystem::Global};
    };
-}
+} // namespace sec21::structural_analysis
 
 #include <nlohmann/json.hpp>
 
 namespace sec21::structural_analysis
 {
    NLOHMANN_JSON_SERIALIZE_ENUM(ActionType, {
-      {Permanent, "Permanent"},
-      {Variable, "Variable"},
-      {Accidental, "Accidental"},
-   })
+                                               {ActionType::Permanent, "Permanent"},
+                                               {ActionType::Variable, "Variable"},
+                                               {ActionType::Accidental, "Accidental"},
+                                            })
 
    NLOHMANN_JSON_SERIALIZE_ENUM(LoadType, {
-      {SelfWeight, "SelfWeight"},
-      {Others, "Others"},
-      {Prestress, "Prestress"},
-      {Dynamic, "Dynamic"},
-      {Static, "Static"},
-      {Temperature, "Temperature"},
-      {Wind, "Wind"},
-      {Snow, "Snow"},
-      {Maintenance, "Maintenance"},
-      {Fire, "Fire"},
-      {Moving, "Moving"},
-      {Dynamic, "Dynamic"},
-      {Seismic, "Seismic"},
-      {Standard, "Standard"},
-   })
+                                             {LoadType::SelfWeight, "SelfWeight"},
+                                             {LoadType::Others, "Others"},
+                                             {LoadType::Prestress, "Prestress"},
+                                             {LoadType::Dynamic, "Dynamic"},
+                                             {LoadType::Static, "Static"},
+                                             {LoadType::Temperature, "Temperature"},
+                                             {LoadType::Wind, "Wind"},
+                                             {LoadType::Snow, "Snow"},
+                                             {LoadType::Maintenance, "Maintenance"},
+                                             {LoadType::Fire, "Fire"},
+                                             {LoadType::Moving, "Moving"},
+                                             {LoadType::Dynamic, "Dynamic"},
+                                             {LoadType::Seismic, "Seismic"},
+                                             {LoadType::Standard, "Standard"},
+                                          })
 
    NLOHMANN_JSON_SERIALIZE_ENUM(LoadDuration, {
-      {Long, "Long"},
-      {Medium, "Medium"},
-      {Short, "Short"},
-      {Instantaneous, "Instantaneous"},
-   })
+                                                 {LoadDuration::Long, "Long"},
+                                                 {LoadDuration::Medium, "Medium"},
+                                                 {LoadDuration::Short, "Short"},
+                                                 {LoadDuration::Instantaneous, "Instantaneous"},
+                                              })
 
    NLOHMANN_JSON_SERIALIZE_ENUM(CoordinateSystem, {
-      {Global, "Global"},
-      {Local, "Local"},
-   })
+                                                     {CoordinateSystem::Global, "Global"},
+                                                     {CoordinateSystem::Local, "Local"},
+                                                  })
 
    template <typename System>
-   void to_json(nlohmann::json &j, temperature_load<System> const &obj)
+   void to_json(nlohmann::json& j, temperature_load<System> const& obj)
    {
-      j = nlohmann::json{
-          {"member_id", obj.member_id},
-          {"delta_t", obj.delta_t},
-          {"alpha_t", obj.alpha_t}};
+      j = nlohmann::json{{"member_id", obj.member_id}, {"delta_t", obj.delta_t}, {"alpha_t", obj.alpha_t}};
    }
 
    template <typename System>
-   void from_json(nlohmann::json const &j, temperature_load<System> &obj)
+   void from_json(nlohmann::json const& j, temperature_load<System>& obj)
    {
       j.at("member_id").get_to(obj.member_id);
       j.at("delta_t").get_to(obj.delta_t);
@@ -238,19 +205,17 @@ namespace sec21::structural_analysis
    }
 
    template <typename System>
-   void to_json(nlohmann::json &j, loadcase<System> const &obj)
+   void to_json(nlohmann::json& j, loadcase<System> const& obj)
    {
-      j = nlohmann::json{
-          {"name", obj.name},
-          {"node_load", obj.node_load},
-          {"temperature_loads", obj.temperature_loads}};
+      j =
+         nlohmann::json{{"name", obj.name}, {"node_load", obj.node_load}, {"temperature_loads", obj.temperature_loads}};
    }
 
    template <typename System>
-   void from_json(nlohmann::json const &j, loadcase<System> &obj)
+   void from_json(nlohmann::json const& j, loadcase<System>& obj)
    {
       j.at("name").get_to(obj.name);
       j.at("node_load").get_to(obj.node_load);
       j.at("temperature_loads").get_to(obj.temperature_loads);
    }
-}
+} // namespace sec21::structural_analysis
