0a380376a3a9c4a93d77f155dbc713c4fd5389da
michael miller <miller.michael@gmx.de>
2021-08-17 20:22:29 +0200

    add conditional noexcept specification

--- include/sec21/for_each_indexed.h
+++ include/sec21/for_each_indexed.h
@@ -1,26 +1,32 @@
 #pragma once
 
+#include <functional>
 #include <iterator>
 
-namespace sec21 
+namespace sec21
 {
    template <typename Iterator, typename Callable>
-   auto for_each_indexed(Iterator first, Iterator last, typename std::iterator_traits<Iterator>::difference_type initial, Callable func)
+   auto for_each_indexed(
+      Iterator first, Iterator last, typename std::iterator_traits<Iterator>::difference_type initial,
+      Callable
+         func) noexcept(noexcept(std::is_nothrow_invocable_v<Callable,
+                                                             typename std::iterator_traits<Iterator>::difference_type,
+                                                             typename std::iterator_traits<Iterator>::value_type>))
    {
-      for (;first != last; ++first, ++initial)
+      for (; first != last; ++first, ++initial)
          std::invoke(func, initial, *first);
       return func;
    }
 
    template <typename Iterator, typename Callable>
-   auto for_each_indexed(Iterator first, Iterator last, Callable func)
+   auto for_each_indexed(Iterator first, Iterator last, Callable func) //! \todo noexcept?
    {
       return for_each_indexed(first, last, 0, func);
    }
 
    template <typename Sequence, typename Callable>
-   auto for_each_indexed(Sequence const& seq, Callable&& func)
+   auto for_each_indexed(Sequence const& seq, Callable&& func)  //! \todo noexcept?
    {
       return for_each_indexed(std::begin(seq), std::end(seq), 0, std::forward<Callable>(func));
-   }   
-}
+   }
+} // namespace sec21
