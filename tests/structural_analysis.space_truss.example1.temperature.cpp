faf3110655517709caa29bd1e2527fd8491cc212
michael miller <miller.michael@gmx.de>
2021-08-18 14:07:21 +0200

    fix test

--- tests/structural_analysis.space_truss.example1.temperature.cpp
+++ tests/structural_analysis.space_truss.example1.temperature.cpp
@@ -1,12 +1,10 @@
 ﻿#include <catch.hpp>
-#include "approx_equal.h"
 
 #include <sec21/file_loader.h>
 #include <sec21/structural_analysis/node.h>
 #include <sec21/structural_analysis/member.h>
 #include <sec21/structural_analysis/space_truss.h>
 #include <sec21/structural_analysis/loadcase.h>
-#include <sec21/structural_analysis/system_result.h>
 #include <sec21/structural_analysis/solve.h>
 #include <sec21/structural_analysis/solver/backend/viennacl.h>
 
@@ -23,51 +21,50 @@ TEST_CASE("example system 1.0 with temperature load", "[sec21][structural_analys
    using space_truss_t = space_truss<node_t, member_t>;
 
    auto sys = sec21::load_from_json<space_truss_t>("example_1.json");
-   REQUIRE(std::size(sys.nodes) == 4);
-   REQUIRE(std::size(sys.members) == 6);
+
+   REQUIRE(size(sys.nodes) == 4);
+   REQUIRE(size(sys.members) == 6);
+   REQUIRE(size(sys.coincidence_table) == 6);
 
    using loadcase_t = loadcase<decltype(sys)>;
    loadcase_t lf1;
    lf1.name = "LF1"s;
    lf1.temperature_loads.emplace_back(loadcase_t::temperature_load_t{1, 30.0_K, 0.012});
 
-   auto [success, result] = solve<solver::backend::viennacl_impl>(sys, lf1);
-   REQUIRE(success == true);
+   SECTION("create vector F from temperature loadcase")
+   {
+      constexpr auto dim = decltype(sys)::dimension_v;
 
-   std::vector<double> flat_displacement{};
-   for (auto [k,v] : result.node) 
-      std::transform(
-         std::begin(v.displacement), 
-         std::end(v.displacement), 
-         std::back_inserter(flat_displacement),
-         [](auto&& e) { return e.value(); });
+      using namespace sec21;
+      using value_t = units::quantity<units::newton, typename decltype(sys)::precision_t>;
 
-   std::vector<double> copied_results{};
-   std::transform(
-      std::begin(result.members), 
-      std::end(result.members), 
-      std::back_inserter(copied_results), 
-      [](auto&& m) { return m.normal_force.value(); });
+      std::vector<value_t> F(std::size(sys.nodes) * dim);
+      REQUIRE(std::size(F) == 8);
+
+      add_temperature_load(sys, lf1, begin(F));
+
+      const auto expected = std::array{ -30'240.0_N, 0.0_N, 30'240.0_N, 0.0_N, 0.0_N, 0.0_N, 0.0_N, 0.0_N };
+      REQUIRE(std::size(F) == std::size(expected));
+      REQUIRE(std::equal(std::begin(F), std::end(F), std::begin(expected)));
+   }
+
+   const auto success = solve<solver::backend::viennacl_impl>(sys, lf1);
+   REQUIRE(success.has_value() == true);
+
+   const auto result = success.value();
 
    // unit: millimeter [mm]
-   REQUIRE(flat_displacement[0] == Approx(-0.54));
-   REQUIRE(flat_displacement[1] == Approx(-0.112).epsilon(kDivergence));
-   REQUIRE(flat_displacement[2] == Approx(0.428).epsilon(kDivergence));
-   REQUIRE(flat_displacement[3] == Approx(-0.112).epsilon(kDivergence));
-   REQUIRE(flat_displacement[4] == Approx(-0.112).epsilon(kDivergence));
+   REQUIRE(std::get<0>(result.nodes[0].displacement).value() == Approx(-0.54).epsilon(kDivergence));
+   REQUIRE(std::get<1>(result.nodes[0].displacement).value() == Approx(-0.112).epsilon(kDivergence));
+   REQUIRE(std::get<0>(result.nodes[1].displacement).value() == Approx(0.428).epsilon(kDivergence));
+   REQUIRE(std::get<1>(result.nodes[1].displacement).value() == Approx(-0.112).epsilon(kDivergence));
+   REQUIRE(std::get<0>(result.nodes[2].displacement).value() == Approx(-0.112).epsilon(kDivergence));
 
    // unit: newton [N]
-   REQUIRE(copied_results[0] == Approx(271'108).epsilon(kDivergence));
-   //! \todo nachlagern / superpposionieren REQUIRE(copied_results[0] == Approx(-31'310).epsilon(kDivergence));
-   REQUIRE(copied_results[1] == Approx(-31'300).epsilon(kDivergence));
-   REQUIRE(copied_results[2] == Approx(-31'300).epsilon(kDivergence));
-   //! \todo REQUIRE(copied_results[3] == Approx(5'000).epsilon(kDivergence));
-   REQUIRE(copied_results[4] == Approx(44'300).epsilon(kDivergence));
-   REQUIRE(copied_results[5] == Approx(44'300).epsilon(kDivergence));
-
-   {
-      std::ofstream ofs{"output_example_1_temp_result.json"};
-      nlohmann::json tmp = result;
-      ofs << std::setw(4) << tmp;
-   }
+   REQUIRE(result.members[0].normal_force.value() == Approx(27'108).epsilon(kDivergence));
+   REQUIRE(result.members[1].normal_force.value() == Approx(-3'130).epsilon(kDivergence));
+   REQUIRE(result.members[2].normal_force.value() == Approx(-3'130).epsilon(kDivergence));
+   REQUIRE(result.members[3].normal_force.value() == Approx(-3'130).epsilon(kDivergence));
+   REQUIRE(result.members[4].normal_force.value() == Approx(4'430).epsilon(kDivergence));
+   REQUIRE(result.members[5].normal_force.value() == Approx(4'430).epsilon(kDivergence));
 }
