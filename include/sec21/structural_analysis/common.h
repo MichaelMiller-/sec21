1e0b316c197755cc50150dfb5281810781ed8fcb
michael miller <miller.michael@gmx.de>
2021-08-18 14:25:32 +0200

    add nodiscard attribute

--- include/sec21/structural_analysis/common.h
+++ include/sec21/structural_analysis/common.h
@@ -1,15 +1,17 @@
 #pragma once
 
+#include <sec21/units/dimensions/area.h>
+#include <sec21/units/dimensions/length.h>
+#include <sec21/units/dimensions/pressure.h>
 #include <sec21/units/quantity.h>
 
 namespace sec21::structural_analysis
 {
    template <typename Precision>
-   inline auto EA_over_l(
-      units::quantity<units::kilopascal, Precision> const& E,
-      units::quantity<units::square_meter, Precision> const& A,
-      units::quantity<units::meter, Precision> const& length)
+   [[nodiscard]] auto EA_over_l(units::quantity<units::kilopascal, Precision> const& E,
+                                units::quantity<units::square_meter, Precision> const& A,
+                                units::quantity<units::meter, Precision> const& length)
    {
       return E * A / length;
    }
-}
+} // namespace sec21::structural_analysis
