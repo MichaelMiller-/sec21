46c3883aab85be82772ba92fd292610691997820
michael miller <miller.michael@gmx.de>
2021-08-18 15:18:46 +0200

    replace hardcoded type "int"

--- include/sec21/row_col.h
+++ include/sec21/row_col.h
@@ -21,19 +21,20 @@ namespace sec21
 
    row& operator++(row& r) noexcept
    {
-      using ut = typename std::underlying_type<row>::type;
+      using ut = std::underlying_type<row>::type;
       r = static_cast<row>(static_cast<ut>(r) + 1);
       return r;
    }
 
    template <typename Iterator>
-#ifdef __cpp_concepts   
+#ifdef __cpp_concepts
    requires std::is_same_v<typename std::iterator_traits<Iterator>::value_type, row>
 #endif
    auto row_to_col(Iterator first, Iterator last) noexcept
    {
+      using ut = typename std::underlying_type<typename std::iterator_traits<Iterator>::value_type>::type;
       std::vector<col> result;
-      std::transform(first, last, std::back_inserter(result), [](auto&& r) { return col{static_cast<int>(r)}; });
+      std::transform(first, last, std::back_inserter(result), [](auto&& r) { return col{static_cast<ut>(r)}; });
       return result;
    }
 } // namespace sec21
