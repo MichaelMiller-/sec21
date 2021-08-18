29546f27f3d9bb94b12aa852a50cae9197496385
michael miller <miller.michael@gmx.de>
2021-08-18 20:22:18 +0200

    add overload

--- include/sec21/structural_analysis/solve.h
+++ include/sec21/structural_analysis/solve.h
@@ -30,6 +30,56 @@ namespace sec21::structural_analysis
 {
    namespace impl
    {
+      template <typename Solver, typename Precision, typename Allocator, typename ProcessResult>
+      [[nodiscard]] auto solve(std::size_t num_nodes,
+                               boost::numeric::ublas::matrix<Precision, boost::numeric::ublas::row_major, Allocator> K,
+                               boost::numeric::ublas::vector<Precision, Allocator> F, std::vector<row> lookup,
+                               std::vector<bool> support_mask, ProcessResult&& process_result)
+      {
+         if (size(lookup) != size(support_mask))
+            throw std::runtime_error("solve: mismatch sizes");
+
+         if (K.size1() != F.size())
+            throw std::runtime_error("solve: K + F size mismatch");
+
+         std::decay_t<decltype(lookup)> supported_rows{};
+         std::decay_t<decltype(lookup)> not_supported_rows{};
+
+         partition_lookup(begin(lookup), end(lookup), begin(support_mask), end(support_mask),
+                          std::back_inserter(supported_rows), std::back_inserter(not_supported_rows));
+
+         const auto supported_cols = row_to_col(begin(supported_rows), end(supported_rows));
+
+         //! \todo use std::vector
+         const auto K_without_supports = numeric::drop(numeric::drop(K, supported_rows), supported_cols);
+         const auto K_strich = numeric::drop(numeric::drop(K, not_supported_rows), supported_cols);
+         const auto F_without_supports = numeric::drop(F, supported_rows);
+
+         // consider sanity checks:
+         // F.size1() == K_without_supports.size1() == K_without_supports.size2()
+         // F.size1() == K_strich.size2()
+         // expects(std::size(lookup) == n * dim);
+
+         const auto verschiebungen = Solver::displacement(K_without_supports, F_without_supports);
+
+         using precision_t = Precision; // typename System::precision_t;
+         boost::numeric::ublas::vector<precision_t> auflagerreaktionen(K_strich.size1());
+         boost::numeric::ublas::axpy_prod(K_strich, verschiebungen, auflagerreaktionen, false);
+
+         constexpr auto dim = 2; // System::dimension_v;
+         std::vector<precision_t> support_reactions(num_nodes * dim, 0.0);
+         std::vector<precision_t> displacements(num_nodes * dim, 0.0);
+
+         zip([&support_reactions](
+                auto&& tuple) { support_reactions[static_cast<size_t>(std::get<row>(tuple))] = std::get<1>(tuple); },
+             std::begin(supported_rows), std::end(supported_rows), std::begin(auflagerreaktionen));
+         zip([&displacements](
+                auto&& tuple) { displacements[static_cast<size_t>(std::get<row>(tuple))] = std::get<1>(tuple); },
+             std::begin(not_supported_rows), std::end(not_supported_rows), std::begin(verschiebungen));
+
+         return std::invoke(process_result, displacements, support_reactions);
+      }
+
       template <typename Solver, typename System, typename Allocator>
       [[nodiscard]] auto
       solve(System const& sys, std::size_t num_nodes,
@@ -71,9 +121,11 @@ namespace sec21::structural_analysis
          std::vector<precision_t> support_reactions(num_nodes * dim, 0.0); //! \todo not saved in result!
          std::vector<precision_t> displacements(num_nodes * dim, 0.0);
 
-         zip([&support_reactions](
-                auto&& tuple) { support_reactions[static_cast<std::size_t>(std::get<row>(tuple))] = std::get<1>(tuple); },
-             std::begin(supported_rows), std::end(supported_rows), std::begin(auflagerreaktionen));
+         zip(
+            [&support_reactions](auto&& tuple) {
+               support_reactions[static_cast<std::size_t>(std::get<row>(tuple))] = std::get<1>(tuple);
+            },
+            std::begin(supported_rows), std::end(supported_rows), std::begin(auflagerreaktionen));
          zip([&displacements](
                 auto&& tuple) { displacements[static_cast<std::size_t>(std::get<row>(tuple))] = std::get<1>(tuple); },
              std::begin(not_supported_rows), std::end(not_supported_rows), std::begin(verschiebungen));
@@ -102,8 +154,8 @@ namespace sec21::structural_analysis
          std::decay_t<decltype(lookup)> supported_rows{};
          std::decay_t<decltype(lookup)> not_supported_rows{};
 
-         partition_lookup(begin(lookup), end(lookup), begin(mask), end(mask),
-                          std::back_inserter(supported_rows), std::back_inserter(not_supported_rows));
+         partition_lookup(begin(lookup), end(lookup), begin(mask), end(mask), std::back_inserter(supported_rows),
+                          std::back_inserter(not_supported_rows));
 
          const auto supported_cols = row_to_col(begin(supported_rows), end(supported_rows));
 
@@ -167,8 +219,8 @@ namespace sec21::structural_analysis
                              });
 
          return make_result(sys, displacements, support_reactions);
-
       }
+
    } // namespace impl
 
    template <typename Solver, typename System>
