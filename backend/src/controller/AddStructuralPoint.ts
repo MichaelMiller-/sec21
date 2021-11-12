32bb52c4bf8ad14dc8d6f48bae49b60ad375dc4a
MichaelMiller- <miller.michael@gmx.de>
2021-11-12 21:36:33 +0100

    remove sanity checks

--- backend/src/controller/AddStructuralPoint.ts
+++ backend/src/controller/AddStructuralPoint.ts
@@ -17,38 +17,12 @@ export async function addStructuralPoint(request: Request, response: Response) {
         return;
     }
 
-    // parse json request -> StructuralPoint
     let pt = new StructuralPoint();
     pt.name = request.body.name;
     pt.coordinate_x = request.body.coordinate_x;
     pt.coordinate_y = request.body.coordinate_y;
     pt.coordinate_z = request.body.coordinate_z;
     pt.project = prj;
-
-    // sanity checks
-    //! \todo cannot the database do the work?
-    if (pt.name.length === 0) {
-        result.success = false;
-        result.message = "'name' cannot be empty"
-        response.send(result);
-        return;
-    }
-
-    const foundPoints = await getManager()
-        .getRepository(StructuralPoint)
-        .createQueryBuilder("structural_point")
-        .where("structural_point.project = :id", { id: request.params.id })
-        .andWhere("structural_point.coordinate_x = :x", { x: pt.coordinate_x })
-        .andWhere("structural_point.coordinate_y = :y", { y: pt.coordinate_y })
-        .andWhere("structural_point.coordinate_z = :z", { z: pt.coordinate_z })
-        .getMany();
-
-    if (foundPoints.length > 0) {
-        result.success = false;
-        result.message = "There is already a Structual Point at this position"
-        response.send(result);
-        return;
-    }
 
     getManager().getRepository(StructuralPoint)
         .save(pt)
@@ -58,7 +32,7 @@ export async function addStructuralPoint(request: Request, response: Response) {
         })
         .catch(ex => {
             result.success = false;
-            result.message = ex;
+            result.message = ex.detail;
         });
 
     response.send(result);
