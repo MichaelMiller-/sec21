3ce3061fba43920212167e264421f1990eadb924
MichaelMiller- <miller.michael@gmx.de>
2021-11-24 11:59:14 +0100

    remove concept

--- include/sec21/structural_analysis/impl/coincidence_matrix.h
+++ include/sec21/structural_analysis/impl/coincidence_matrix.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <sec21/numeric/make_matrix.h>
-#include <sec21/structural_analysis/concepts.h>
 
 #include <algorithm>
 #include <memory>
@@ -12,23 +11,18 @@
 
 namespace sec21::structural_analysis::impl
 {
-#ifdef __cpp_concepts
-   template <typename Allocator, SpaceTruss2D System>
-#else
    template <typename Allocator, typename System>
-#endif
    [[nodiscard]] auto coincidence_matrix(System const& sys, typename System::member_descriptor_t id)
    {
-      static_assert(System::node_t::dimension_v == 2, "Only works with 2D system");
+      constexpr auto dim = System::dimension_v;
+      static_assert(dim == 2, "Only works with 2D system");
 
-      // sanitiy check
+      // sanity check
       if (size(sys.members) != size(sys.coincidence_table))
          throw std::runtime_error("coincidence_matrix: size of members mismatches with size of coincidence table");
 
-      constexpr auto dim = System::node_t::dimension_v;
-
       auto find_node = [&sys](auto const& id) {
-         const auto it = std::find_if(begin(sys.nodes), end(sys.nodes), [&id](auto&& e) { return id == e.id; });
+         const auto it = std::find_if(begin(sys.nodes), end(sys.nodes), [&id](auto const& e) { return id == e.id; });
          if (it == end(sys.nodes))
 #ifdef __cpp_lib_format
             throw std::runtime_error(std::format("coincidence_matrix: could not find node id {}", id));
@@ -39,13 +33,13 @@ namespace sec21::structural_analysis::impl
       };
 
       const auto n = 4; // anzahl der verschiebungen
-      const auto m = std::size(sys.nodes) * dim;
+      const auto m = size(sys.nodes) * dim;
       //! \todo can throw
       const auto [s, e] = sys.coincidence_table.at(id);
       const auto from = find_node(s);
       const auto to = find_node(e);
-      const auto u = std::distance(std::begin(sys.nodes), from);
-      const auto v = std::distance(std::begin(sys.nodes), to);
+      const auto u = std::distance(begin(sys.nodes), from);
+      const auto v = std::distance(begin(sys.nodes), to);
 
       auto result = numeric::make_matrix<typename std::allocator_traits<Allocator>::value_type, Allocator>(n, m, 0);
       result(0, (u * dim) + 0) = 1;
