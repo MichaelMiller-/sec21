71dc14a716c9b1423dc15b794c6b388096439605
MichaelMiller- <miller.michael@gmx.de>
2021-11-26 11:03:33 +0100

    disable overloads

--- tests/approx_equal.h
+++ tests/approx_equal.h
@@ -4,35 +4,32 @@
 
 #include <algorithm>
 
-#if defined(CATCH_VERSION_MAJOR) // && defined(CATCH_VERSION_MINOR)
+// #if defined(CATCH_VERSION_MAJOR) // && defined(CATCH_VERSION_MINOR)
 
-   class approximately_equal
+class approximately_equal
+{
+   double deviation{};
+
+ public:
+   //! \param epsilon 0.1 ... 0.0000000001
+   explicit approximately_equal(double epsilon = 0.001) : deviation{epsilon} {}
+
+   template <typename T>
+   bool operator()(T const& lhs, T const& rhs)
    {
-      double deviation{};
-   public:
-      //! \param epsilon 0.1 ... 0.0000000001
-      explicit approximately_equal(double epsilon = 0.001) 
-         : deviation{ epsilon } 
-      {}
+      return Approx(lhs).epsilon(deviation) == rhs;
+   }
+};
 
-      template <typename T>
-      bool operator()(T const& lhs, T const& rhs)
-      {
-         return Approx(lhs).epsilon(deviation) == rhs;
-      }
-   };
-
-#endif
-
+// #endif
 
 class logically_equal
 {
    double deviation{};
-public:
+
+ public:
    //! \param epsilon 0.1 ... 0.0000000001
-   explicit logically_equal(double epsilon = 0.001) 
-      : deviation{ epsilon } 
-   {}
+   explicit logically_equal(double epsilon = 0.001) : deviation{epsilon} {}
 
    template <typename T>
    bool operator()(T const& lhs, T const& rhs) noexcept
@@ -40,33 +37,31 @@ public:
       return std::fabs(lhs - rhs) < deviation;
    }
 };
-
+#if 0
 template <typename Iterator1, typename Iterator2, typename Compare>
-bool approx_equal(Iterator1 first1, Iterator1 last1, Iterator2 first2, Compare&& compare) 
-//! \todo noexcept(std::invoke(compare, Iterator1::value_type{}))
+bool approx_equal(Iterator1 first1, Iterator1 last1, Iterator2 first2, Compare&& compare) noexcept(
+   noexcept(std::is_nothrow_invocable_v<Compare, typename std::iterator_traits<Iterator1>::value_type,
+                                        typename std::iterator_traits<Iterator2>::value_type>))
 {
-   return std::equal(first1, last1, first2, compare); 
+   return std::equal(first1, last1, first2, compare);
 }
+//! \todo implement high-order function
+template <typename Rng1, typename Rng2, typename Compare>
+bool approx_equal(Rng1&& rng1, Rng2&& rng2, Compare&& compare)
+{
+   return approx_equal(begin(rng1), end(rng1), begin(rng2), std::forward<Compare>(compare));
+}
+#endif
 
 template <typename Iterator1, typename Iterator2>
 bool approx_equal(Iterator1 first1, Iterator1 last1, Iterator2 first2, double deviation) noexcept
 {
-   return std::equal(
-      first1, 
-      last1, 
-      first2, 
-      [deviation](auto const& lhs, auto const& rhs) { return Approx(lhs).epsilon(deviation) == rhs; }); 
-}
-
-//! \todo implement high-order function 
-template <typename Rng1, typename Rng2, typename Compare>
-bool approx_equal(Rng1 const& rng1, Rng2 const& rng2, Compare&& compare) noexcept
-{
-   return approx_equal(std::begin(rng1), std::end(rng1), std::begin(rng2), std::forward<Compare>(compare));
+   return std::equal(first1, last1, first2,
+                     [deviation](auto const& lhs, auto const& rhs) { return Approx(lhs).epsilon(deviation) == rhs; });
 }
 
 template <typename Rng1, typename Rng2>
-bool approx_equal(Rng1 const& rng1, Rng2 const& rng2, double deviation) noexcept
+bool approx_equal(Rng1&& rng1, Rng2&& rng2, double deviation)
 {
-   return std::equal(std::begin(rng1), std::end(rng1), std::begin(rng2), approximately_equal{ deviation });
+   return std::equal(begin(rng1), end(rng1), begin(rng2), approximately_equal{deviation});
 }
