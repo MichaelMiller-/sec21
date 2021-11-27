d18c8fdb6463db821d57551568e5e5c95233f185
MichaelMiller- <miller.michael@gmx.de>
2021-11-27 18:12:51 +0100

    initialize member with now()

--- backend/src/controller/AddProject.ts
+++ backend/src/controller/AddProject.ts
@@ -7,26 +7,14 @@ export async function addProject(request: Request, response: Response) {
 
     let result = new Result<null>();
 
-    let prj = new Project();
-    prj.name = request.body.name;
-    prj.title = request.body.title;
-
-    if (prj.name.length === 0) {
-        result.success = false;
-        result.message = "'name' cannot be empty"
-        response.send(result);
-        return;
-    }
-
-    if (prj.title.length === 0) {
-        result.success = false;
-        result.message = "'title' cannot be empty"
-        response.send(result);
-        return;
-    }
+    let obj = new Project();
+    obj.name = request.body.name;
+    obj.title = request.body.title;
+    obj.createdAt = new Date(Date.now());
+    obj.modifiedAt = new Date(Date.now());
 
     getManager().getRepository(Project)
-        .save(prj)
+        .save(obj)
         .then(() => {
             result.success = true;
             result.message = "";
