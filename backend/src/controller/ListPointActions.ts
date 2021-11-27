5b1cd276d8befef29129c8cc99e92b7192a943c4
MichaelMiller- <miller.michael@gmx.de>
2021-11-27 19:25:11 +0100

    join reference point

--- backend/src/controller/ListPointActions.ts
+++ backend/src/controller/ListPointActions.ts
@@ -1,15 +1,16 @@
-import { Request, Response } from "express";
-import { getManager } from "typeorm";
-import { PointAction } from "../entity/PointAction";
+import {Request, Response} from "express";
+import {getManager} from "typeorm";
+import {PointAction} from "../entity/PointAction";
 
 export async function listPointActions(request: Request, response: Response) {
 
-   const values = await getManager()
-      .getRepository(PointAction)
-      .createQueryBuilder("obj")
-      .leftJoinAndSelect('obj.loadcase', 'lc')
-      .where("lc.project = :id", { id: request.params.id })
-      .getMany();
+    const values = await getManager()
+        .getRepository(PointAction)
+        .createQueryBuilder("obj")
+        .leftJoinAndSelect('obj.referencePoint', 'notused')
+        .leftJoinAndSelect('obj.loadcase', 'lc')
+        .where("lc.project = :id", {id: request.params.id})
+        .getMany();
 
-   response.send(values);
+    response.send(values);
 }
