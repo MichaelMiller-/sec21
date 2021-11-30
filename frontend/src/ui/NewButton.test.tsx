f85860eb12bcfeaf1eb08c7ab7a283db1a2c6378
MichaelMiller- <miller.michael@gmx.de>
2021-11-30 09:37:09 +0100

    fix test

--- frontend/src/ui/NewButton.test.tsx
+++ frontend/src/ui/NewButton.test.tsx
@@ -3,19 +3,19 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import NewButton from "./NewButton";
 
 it('renders without crashing', async () => {
-   render(<NewButton onClick={() => { }} disabled={false} />);
+   render(<NewButton onClick={() => { }} disabled={false} title={"test123"}/>);
    expect(screen.getByRole('button')).toBeInTheDocument();
    expect(screen.getByRole('button')).not.toBeDisabled()
 })
 
 it('disable the button correctly', async () => {
-   render(<NewButton onClick={() => { }} disabled={true} />);
+   render(<NewButton onClick={() => { }} disabled={true} title={"test123"} />);
    expect(screen.getByRole('button')).toBeDisabled()
 })
 
 it('handles the click', async () => {
    let value = 0;
-   render(<NewButton onClick={() => { value = 42; }} disabled={true} />);
+   render(<NewButton onClick={() => { value = 42; }} disabled={true} title={"test123"} />);
 
    fireEvent.click(screen.getByRole('button'))
    expect(value === 42);
