cf709a8e35d36ab2e388fea3aa9cc2329347cf18
MichaelMiller- <miller.michael@gmx.de>
2021-12-04 11:41:44 +0100

    consider SupportTranslation

--- frontend/src/DialogAddPointSupport.tsx
+++ frontend/src/DialogAddPointSupport.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
-import {Button, Col, Form, Modal, Spinner} from "react-bootstrap";
-import {DbPointSupport, DbStructuralPoint, SupportType} from "./DatabaseInterface";
+import {Button, Col, Form, Modal, Row} from "react-bootstrap";
+import {DbPointSupport, DbStructuralPoint, SupportTranslation, SupportType} from "./DatabaseInterface";
 import axios from "axios";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faThumbtack} from "@fortawesome/free-solid-svg-icons";
@@ -19,6 +19,9 @@ const DialogAddPointSupport = (props: DialogAddPointSupportProps) => {
         uuid: "",
         name: "",
         type: SupportType.FIXED,
+        ux: SupportTranslation.FREE,
+        uy: SupportTranslation.FREE,
+        uz: SupportTranslation.FREE,
         referencePoint: {
             id: 0,
             name: "",
@@ -29,9 +32,11 @@ const DialogAddPointSupport = (props: DialogAddPointSupportProps) => {
         }
     });
     const [structurePointList, setStructurePointList] = useState<DbStructuralPoint[]>([]);
-    const [loading, setLoading] = useState(true)
-    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedIndexNode, setSelectedIndexNode] = useState(0);
     const [pinned, setPinned] = useState(false);
+    const [selectedIndexUx, setSelectedIndexUx] = useState(0);
+    const [selectedIndexUy, setSelectedIndexUy] = useState(0);
+    const [selectedIndexUz, setSelectedIndexUz] = useState(0);
 
     const handleInputChange = (e: { target: { name: any; value: any; }; }) => {
         const {name, value} = e.target;
@@ -40,7 +45,14 @@ const DialogAddPointSupport = (props: DialogAddPointSupportProps) => {
 
     const handleSubmit = async (event: { currentTarget: any; preventDefault: () => void; stopPropagation: () => void; }) => {
         event.preventDefault();
-        await axios.post(process.env.REACT_APP_BACKEND + '/pointsupport/' + structurePointList[selectedIndex].id, data)
+
+        // map selected index to enum
+        const keys = Object.keys(SupportTranslation);
+        data.ux = SupportTranslation[keys[selectedIndexUx] as keyof typeof SupportTranslation];
+        data.uy = SupportTranslation[keys[selectedIndexUy] as keyof typeof SupportTranslation];
+        data.uy = SupportTranslation[keys[selectedIndexUz] as keyof typeof SupportTranslation];
+
+        await axios.post(process.env.REACT_APP_BACKEND + '/pointsupport/' + structurePointList[selectedIndexNode].id, data)
         props.onSubmit();
         if (pinned === false)
             props.onHide();
@@ -49,7 +61,6 @@ const DialogAddPointSupport = (props: DialogAddPointSupportProps) => {
     useEffect(() => {
         axios.get(process.env.REACT_APP_BACKEND + '/structuralpoints/' + props.projectId)
             .then(response => setStructurePointList(response.data))
-            .finally(() => setLoading(false))
     }, [props.show, props.projectId]);
 
     return (
@@ -59,24 +70,63 @@ const DialogAddPointSupport = (props: DialogAddPointSupportProps) => {
             </Modal.Header>
             <Form onSubmit={handleSubmit}>
                 <Modal.Body>
-                    <Form.Group as={Col} md="4" controlId="validationPointSupportName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control
-                            required
-                            type="text"
-                            name="name"
-                            value={data.name}
-                            onChange={handleInputChange}
-                        />
-                        <Form.Control.Feedback>Please provide a valid name.</Form.Control.Feedback>
-                    </Form.Group>
-                    {loading && <Spinner animation="border" role="status"></Spinner>}
-                    <Form.Group controlId="ControlSelectBegin">
-                        <Form.Label>attached</Form.Label>
-                        <Form.Control as="select" onChange={(e: any) => { setSelectedIndex(e.target.selectedIndex); }}>
-                            {structurePointList.map(e => (<option key={e.id}>{e.name}</option>))}
-                        </Form.Control>
-                    </Form.Group>
+                    <Row>
+                        <Col xs={7}>
+                            <Form.Group controlId="validationPointSupportName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control
+                                    required
+                                    type="text"
+                                    name="name"
+                                    value={data.name}
+                                    onChange={handleInputChange}
+                                />
+                                <Form.Text id="nameHelpBlock" muted>
+                                    Name of the Support
+                                </Form.Text>
+                                <Form.Control.Feedback>Please provide a valid name.</Form.Control.Feedback>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="controlSelectSupported">
+                                <Form.Label>attached</Form.Label>
+                                <Form.Control as="select" onChange={(e: any) => {
+                                    setSelectedIndexNode(e.target.selectedIndex);
+                                }}>
+                                    {structurePointList.map(e => (<option key={e.id}>{e.name}</option>))}
+                                </Form.Control>
+                                <Form.Text id="selectSupportedNodeBlock" muted>
+                                    Attached to Node
+                                </Form.Text>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <Form.Label>ux</Form.Label>
+                            <Form.Control as="select" onChange={(e: any) => { setSelectedIndexUx(e.target.selectedIndex); }}>
+                                { /* TODO: use Object.values(SupportTranslation).map(....); */ }
+                                <option key={0}>rigid</option>
+                                <option key={1}>free</option>
+                            </Form.Control>
+                        </Col>
+                        <Col>
+                            <Form.Label>uy</Form.Label>
+                            <Form.Control as="select" onChange={(e: any) => { setSelectedIndexUy(e.target.selectedIndex); }}>
+                                { /* TODO: use Object.values(SupportTranslation).map(....); */ }
+                                <option key={0}>rigid</option>
+                                <option key={1}>free</option>
+                            </Form.Control>
+                        </Col>
+                        <Col>
+                            <Form.Label>uz</Form.Label>
+                            <Form.Control as="select" onChange={(e: any) => { setSelectedIndexUz(e.target.selectedIndex); }}>
+                                { /* TODO: use Object.values(SupportTranslation).map(....); */ }
+                                <option key={0}>rigid</option>
+                                <option key={1}>free</option>
+                            </Form.Control>
+                        </Col>
+                    </Row>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="link" onClick={() => {
