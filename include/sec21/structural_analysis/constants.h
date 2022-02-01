ca86b94871af0360f0ef44281a26e354f5d41edf
MichaelMiller- <miller.michael@gmx.de>
2022-02-01 16:08:18 +0100

    new constant: half_pi and quarter_pi

--- include/sec21/structural_analysis/constants.h
+++ include/sec21/structural_analysis/constants.h
@@ -11,9 +11,18 @@ namespace sec21::structural_analysis
 #ifdef __cpp_lib_math_constants
    template <typename T>
    constexpr auto pi = std::numbers::pi_v<T>;
+
+   template <typename T>
+   constexpr auto half_pi = pi<T> * 0.5;
 #else
    template <typename T>
    constexpr auto pi = boost::math::constants::pi<T>();
+
+   template <typename T>
+   constexpr auto half_pi = boost::math::constants::half_pi<T>();
 #endif
+
+   template <typename T>
+   constexpr auto quarter_pi = half_pi<T> * 0.5;
 
 } // namespace sec21::structural_analysis
