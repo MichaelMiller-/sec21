22d22bf2e8403adbd48e412f601b7a75712403e8
MichaelMiller- <miller.michael@gmx.de>
2022-02-23 19:06:38 +0100

    ensure constant initialization. see c++ standard [basic.start.static]

--- include/sec21/structural_analysis/constants.h
+++ include/sec21/structural_analysis/constants.h
@@ -10,19 +10,19 @@ namespace sec21::structural_analysis
 {
 #ifdef __cpp_lib_math_constants
    template <typename T>
-   constexpr auto pi = std::numbers::pi_v<T>;
+   static constexpr auto pi = std::numbers::pi_v<T>;
 
    template <typename T>
-   constexpr auto half_pi = pi<T> * 0.5;
+   static constexpr auto half_pi = pi<T> * 0.5;
 #else
    template <typename T>
-   constexpr auto pi = boost::math::constants::pi<T>();
+   static constexpr auto pi = boost::math::constants::pi<T>();
 
    template <typename T>
-   constexpr auto half_pi = boost::math::constants::half_pi<T>();
+   static constexpr auto half_pi = boost::math::constants::half_pi<T>();
 #endif
 
    template <typename T>
-   constexpr auto quarter_pi = half_pi<T> * 0.5;
+   static constexpr auto quarter_pi = half_pi<T> * 0.5;
 
 } // namespace sec21::structural_analysis
