7d1e9926475393f43530d4282e1b3d6a6c89a2d5
MichaelMiller- <miller.michael@gmx.de>
2022-04-08 23:42:34 +0200

    default initialize member

--- logger/src/file_logger.h
+++ logger/src/file_logger.h
@@ -6,7 +6,7 @@ namespace sec21::log
 {
    class file_logger : public base_logger
    {
-      std::map<int, std::vector<entry>> logfiles;
+      std::map<int, std::vector<entry>> logfiles{};
 
     public:
       void log(entry const& e) override;
