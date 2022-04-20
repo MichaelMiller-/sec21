0e46b62a9a659a5d4241a00ee9b9c9a5c4323460
MichaelMiller- <miller.michael@gmx.de>
2022-04-20 20:34:10 +0200

    delete dead code

--- logger/README.md
+++
@@ -1,2 +0,0 @@
-# sec21::log_service
-A static library witch provides a unity log interface
--- logger/src/string_loger.cpp
+++
@@ -1,8 +0,0 @@
-#include "string_loger.h"
-
-using namespace sec21::log;
-
-void string_logger::log(entry const& e)
-{
-   f.insert(e.process_id, e);
-}
