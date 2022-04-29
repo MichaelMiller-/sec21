bc0e8206941ebeb73c267757406f5187ba44485d
MichaelMiller- <miller.michael@gmx.de>
2022-04-29 10:00:11 +0200

    backend moved to an extra project

--- backend/src/controller/GetMaterial.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-import { getManager } from "typeorm";
-import { Material } from "../entity/Material";
-
-export async function getMaterial(request: Request, response: Response) {
-    const values = await getManager().getRepository(Material).findOne(request.params.id);
-    response.send(values);
-}
--- backend/src/dto/Result.ts
+++
@@ -1,5 +0,0 @@
-export default class Result<T> {
-   success: boolean = true;
-   message: string = "";
-   data: T;
-}
