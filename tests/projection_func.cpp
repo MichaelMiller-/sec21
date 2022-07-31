ef7280ee4b5d456bc127902bf997619ffe5e663d
MichaelMiller- <miller.michael@gmx.de>
2022-07-31 22:41:29 +0200

    adjustments to new Catch2 version

--- tests/projection_func.cpp
+++ tests/projection_func.cpp
@@ -1,7 +1,7 @@
 #include <catch.hpp>
 
-#include <vector>
 #include <algorithm>
+#include <vector>
 
 #include <sec21/projection_func.h>
 
@@ -15,40 +15,24 @@ TEST_CASE("projection wrapper function", "[sec21][core]")
       double b;
    };
 
-   std::vector<foo> input{
-      { 1, 3.12 },
-      { 4, 3.12 },
-      { 5, 76.7 },
-      { 3, 7.2 },
-      { 2, 0.82 }
-   };
+   std::vector<foo> input{{1, 3.12}, {4, 3.12}, {5, 76.7}, {3, 7.2}, {2, 0.82}};
 
    SECTION("sort example")
    {
-      std::sort(std::begin(input), std::end(input), projection_func{ std::less{}, &foo::a });
+      std::sort(std::begin(input), std::end(input), projection_func{std::less{}, &foo::a});
 
-      decltype(input) const expected{
-        { 1, 3.12 },
-        { 2, 0.82 },
-        { 3, 7.2 },
-        { 4, 3.12 },
-        { 5, 76.7 }
-      };
+      decltype(input) const expected{{1, 3.12}, {2, 0.82}, {3, 7.2}, {4, 3.12}, {5, 76.7}};
 
-      REQUIRE(std::equal(
-         std::begin(input), 
-         std::end(input), 
-         std::begin(expected), 
-         std::end(expected),
-         // only member foo::a is needed for this test
-         [](auto const& lhs, auto const& rhs){ return lhs.a == rhs.a; }) == true);
+      REQUIRE(std::equal(std::begin(input), std::end(input), std::begin(expected), std::end(expected),
+                         // only member foo::a is needed for this test
+                         [](auto const& lhs, auto const& rhs) { return lhs.a == rhs.a; }) == true);
    }
    SECTION("find example")
    {
-      auto const it = std::find_if(std::begin(input), std::end(input), projection{ std::equal_to{}, &foo::a, 5 }); 
+      auto const it = std::find_if(std::begin(input), std::end(input), projection{std::equal_to{}, &foo::a, 5});
 
       REQUIRE(it != std::end(input));
       REQUIRE(it->a == 5);
-      REQUIRE(it->b == Approx(76.7));
+      REQUIRE(it->b == Catch::Approx(76.7));
    }
 }
