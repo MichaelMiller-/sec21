cd5add0204da663107df10d79dff22f9f7e7e061
MichaelMiller- <miller.michael@gmx.de>
2022-08-03 14:57:59 +0200

    move static-asserts to catch2 test-framework

--- include/sec21/type_traits/is_std_string.h
+++ include/sec21/type_traits/is_std_string.h
@@ -1,25 +1,19 @@
 #pragma once
 
+#include <type_traits>
+
 namespace sec21::type_traits
 {
    namespace detail
    {
       [[maybe_unused]] std::true_type is_std_string(std::string);
       [[maybe_unused]] std::false_type is_std_string(...);
-   }
+   } // namespace detail
 
-   template<typename T>
+   template <typename T>
    using is_std_string = decltype(detail::is_std_string(std::declval<T>()));
 
-   template<typename T>
-   inline constexpr auto is_std_string_v = is_std_string<T>::value;
+   template <typename T>
+   static constexpr auto is_std_string_v = is_std_string<T>::value;
 
-   static_assert(is_std_string<std::string>::value);
-   static_assert(is_std_string<int>::value == false);
-   static_assert(is_std_string<std::wstring>::value == false);
-   static_assert(is_std_string<float>::value == false);
-
-   static_assert(is_std_string_v<std::string>);
-   static_assert(is_std_string_v<float> == false);
-   static_assert(is_std_string_v<int> == false);
-}
+} // namespace sec21::type_traits
