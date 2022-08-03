c54627e724cebeb8a3a497ea1d5492ecd9c3e109
MichaelMiller- <miller.michael@gmx.de>
2022-08-03 14:34:27 +0200

    move static-asserts to catch2 test-framework

--- include/sec21/type_traits/has_type.h
+++ include/sec21/type_traits/has_type.h
@@ -24,25 +24,4 @@ namespace sec21::type_traits
    template <typename T, typename... Ts>
    static constexpr bool has_type_v = has_type<T, Ts...>::value;
 
-   namespace compile_time_tests
-   {
-      static_assert(has_type<int, int, float, char>::value == true);
-      static_assert(has_type<float, int, float, char>::value == true);
-      static_assert(has_type<char, int, float, char>::value == true);
-      static_assert(has_type<double, int, float, char>::value == false);
-
-      using test_tuple_t = std::tuple<int, float, double>;
-
-      static_assert(has_type<int, test_tuple_t>::value == true);
-      static_assert(has_type<float, test_tuple_t>::value == true);
-      static_assert(has_type<double, test_tuple_t>::value == true);
-      static_assert(has_type<char, test_tuple_t>::value == false);
-
-      using test_variant_t = std::variant<int, float, double>;
-
-      static_assert(has_type<int, test_variant_t>::value == true);
-      static_assert(has_type<float, test_variant_t>::value == true);
-      static_assert(has_type<double, test_variant_t>::value == true);
-      static_assert(has_type<char, test_variant_t>::value == false);
-   } // namespace compile_time_tests
 } // namespace sec21::type_traits
