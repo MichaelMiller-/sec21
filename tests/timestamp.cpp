25f4bc94efb32cbe5c69bf46ff074c808640956e
MichaelMiller- <miller.michael@gmx.de>
2022-08-06 09:10:18 +0200

    use 64bit integer

--- tests/timestamp.cpp
+++ tests/timestamp.cpp
@@ -2,9 +2,9 @@
 
 #include <sec21/timestamp.h>
 
-TEST_CASE("try to convert a string into timestamp", "[sec21][core]")
+TEST_CASE("try to convert a string into a timestamp", "[sec21][core]")
 {
    using namespace sec21;
 
-   REQUIRE(timestamp<long>("2001-01-01 23:53:23.777") == 978393203777);
+   REQUIRE(timestamp<std::int64_t>("2001-01-01 23:53:23.777") == 978393203777);
 }
