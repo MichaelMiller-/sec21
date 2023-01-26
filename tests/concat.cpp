f4f1dc7abce2f5a88117276806f14f357b82e5fd
MichaelMiller- <miller.michael@gmx.de>
2023-01-26 09:33:11 +0100

    fix typo

--- tests/concat.cpp
+++ tests/concat.cpp
@@ -4,13 +4,13 @@
 
 struct foo
 {
-   int x{ 0 };
-   int y{ 0 };
+   int x{0};
+   int y{0};
 };
 
-// needed for catch output formater
+// needed for catch output formatter
 template <typename CharT, typename Traits>
-std::basic_ostream<CharT, Traits>& operator << (std::basic_ostream<CharT, Traits>& os, foo const& f) 
+std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, foo const& f)
 {
    return os << '(' << f.x << ", " << f.y << ")";
 }
@@ -19,9 +19,9 @@ TEST_CASE("concat string-convertible objects", "[sec21][core]")
 {
    using namespace sec21;
 
-   foo obj{ 17, 42 };
+   foo obj{17, 42};
    const auto result = concat<char>("string1", 4, "foo", 3.14, obj, "bar");
-   const std::string expected{ "string14foo3.14(17, 42)bar" };
+   const std::string expected{"string14foo3.14(17, 42)bar"};
 
    REQUIRE(result == expected);
 }
