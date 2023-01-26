3f98c2f7e0c422aec6df228304079dc424cf7bb0
MichaelMiller- <miller.michael@gmx.de>
2023-01-26 10:22:59 +0100

    replace in_range() with std::clamp

--- include/sec21/in_range.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace sec21 
-{
-   template <typename T>
-   constexpr bool in_range(T const& value, T const& low, T const& high) noexcept {
-      return value >= low && value <= high;
-   }
-}
