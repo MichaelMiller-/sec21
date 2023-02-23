4be4f005674a1e0595e6501b5076d3508f9bd4ef
MichaelMiller- <miller.michael@gmx.de>
2023-02-23 11:59:23 +0100

    mark the function noexcept if the unary-operation is also noexcept

--- include/sec21/transform.h
+++ include/sec21/transform.h
@@ -7,6 +7,7 @@ namespace sec21
    //! \brief transform a std::tuple<Ts...> into a std::tuple<U>
    template <typename... Ts, typename UnaryOperation>
    [[nodiscard]] constexpr auto transform(std::tuple<Ts...> const& input, UnaryOperation unary_op)
+      noexcept(noexcept(std::is_nothrow_invocable_v<UnaryOperation, Ts...>))
    {
       return std::apply([&](auto const&... value) { return std::make_tuple(unary_op(value)...); }, input);
    }
