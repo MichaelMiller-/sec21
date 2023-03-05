808d34837af0245b5ebcea4538e96bb58cffc5b6
MichaelMiller- <miller.michael@gmx.de>
2023-03-05 10:38:18 +0100

    add nodiscard attribute

--- include/sec21/reflection/get_column.h
+++ include/sec21/reflection/get_column.h
@@ -7,7 +7,7 @@
 namespace sec21::reflection
 {
    template <auto Index, typename T>
-   constexpr decltype(auto) get_column(T&& t) noexcept
+   [[nodiscard]] constexpr decltype(auto) get_column(T&& t) noexcept
    {
       using reflection_t = typename table<std::decay_t<T>>::metainfo;
       static_assert(Index < std::tuple_size_v<reflection_t>, "Index is out of range");
