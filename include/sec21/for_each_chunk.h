7e9b28b8261f0970da4eea3f122f85ed1e23580c
MichaelMiller- <miller.michael@gmx.de>
2023-03-22 13:58:24 +0100

    add overload for a range of values

--- include/sec21/for_each_chunk.h
+++ include/sec21/for_each_chunk.h
@@ -11,29 +11,44 @@ namespace sec21
       {
          static_assert(ChunkSize > 1);
 
-         template <typename Iterator, typename Callable, std::size_t ...Is>
-         constexpr auto invoke(Iterator first, Callable&& func, std::index_sequence<Is...>) const
+         template <typename Callable, typename... Args>
+         constexpr void invoke(Callable&& func, Args&&... args) const
+            noexcept(std::is_nothrow_invocable_v<Callable, Args...>)
          {
-            return std::invoke(std::forward<Callable>(func), *std::next(first, Is)...);
+            std::invoke(std::forward<Callable>(func), std::forward<Args>(args)...);
          }
 
-      public:
+         template <typename Iterator, typename Callable, auto... Is>
+         constexpr void invoke(Iterator first, Callable&& func, std::index_sequence<Is...>) const
+         {
+            invoke(std::forward<Callable>(func), *std::next(first, Is)...);
+         }
+
+       public:
          template <typename Iterator, typename Callable>
          constexpr auto operator()(Iterator first, Iterator last, Callable&& func) const
          {
-            while (std::distance(first, last) >= ChunkSize)
-            {
+            if (auto result = std::distance(first, last) % ChunkSize; result != 0) {
+               throw std::runtime_error("Number of elements cannot be divided by ChunkSize");
+            }
+
+            while (std::distance(first, last) >= ChunkSize) {
                invoke(first, std::forward<Callable>(func), std::make_index_sequence<ChunkSize>{});
                std::advance(first, ChunkSize);
             }
-            //! \todo: if std::distance(first, last) > 0 
          }
       };
-   }
+   } // namespace detail
 
    template <auto ChunkSize, typename Iterator, typename Callable>
    void for_each_chunk(Iterator first, Iterator last, Callable&& func)
    {
       detail::for_each_chunk_impl<ChunkSize>()(first, last, std::forward<Callable>(func));
    }
-}
+
+   template <auto ChunkSize, typename Range, typename Callable>
+   void for_each_chunk(Range&& range, Callable&& func)
+   {
+      for_each_chunk<ChunkSize>(begin(range), end(range), std::forward<Callable>(func));
+   }
+} // namespace sec21
--- tests/for_each_chunk.cpp
+++ tests/for_each_chunk.cpp
@@ -1,30 +1,58 @@
-﻿#define CATCH_CONFIG_MAIN
-#include <catch.hpp>
+﻿#include <catch.hpp>
 
 #include <sec21/for_each_chunk.h>
 
 #include <vector>
-#include <array>
 
 TEST_CASE("apply a function to every chunk in a sequence", "[sec21][core]")
 {
-   const auto input = std::vector{ 1, 2, 3, 4, 5, 6, 7, 8 };
-   using value_t = decltype(input)::value_type;
+   using namespace sec21;
 
-   std::vector<std::pair<value_t, value_t>> result{};
-   sec21::for_each_chunk<2>(
-      std::begin(input), 
-      std::end(input), 
-      [&result](auto value1, auto value2){ return result.push_back({value1, value2}); });
+   SECTION("copy values into a new range")
+   {
+      const auto input = std::vector{ 1, 2, 3, 4, 5, 6, 7, 8 };
+      using value_t = decltype(input)::value_type;
 
+      std::vector<std::pair<value_t, value_t>> result{};
+      for_each_chunk<2>(
+         begin(input),
+         end(input),
+         [&result](auto value1, auto value2){ result.push_back({value1, value2}); });
 
-   const auto expected = std::vector<std::pair<value_t, value_t>>{ 
-      { 1, 2 },
-      { 3, 4 },
-      { 5, 6 },
-      { 7, 8 }
-   };
+      const auto expected = decltype(result){
+         { 1, 2 },
+         { 3, 4 },
+         { 5, 6 },
+         { 7, 8 }
+      };
+      REQUIRE(std::size(result) == std::size(expected));
+      REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)));
+   }
+   SECTION("test with an empty input range")
+   {
+      const auto input = std::vector<int>{};
+      using value_t = decltype(input)::value_type;
 
-   REQUIRE(std::size(result) == std::size(expected));
-   REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)));
+      std::vector<std::pair<value_t, value_t>> result{};
+      for_each_chunk<2>(input, [&result](auto x, auto y){ result.push_back(std::make_pair(x, y)); });
+
+      REQUIRE(std::size(result) == 0);
+   }
+   SECTION("mutate the input range")
+   {
+      auto result = std::vector{ 1, 2, 3, 4, 5, 6, 7, 8 };
+
+      for_each_chunk<2>(result, [](auto& x, auto& y){ x = y; });
+
+      const auto expected = decltype(result){2, 2, 4, 4, 6, 6, 8, 8};
+      REQUIRE(std::size(result) == std::size(expected));
+      REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)));
+   }
+   SECTION("input range cannot be divided by chunk-size")
+   {
+      REQUIRE_THROWS([] {
+         const auto input = std::vector{ 1, 2, 3, 4, 5, 6, 7, 8 };
+         for_each_chunk<3>(input, [](auto, auto, auto){ /* nothing */ });
+      }());
+   }
 }
