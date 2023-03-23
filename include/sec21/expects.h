4dd50ecd542f319c1e8e488749b51ea9fcad0593
MichaelMiller- <miller.michael@gmx.de>
2023-03-23 07:43:04 +0100

    remove std::source_location from header

--- include/sec21/expects.h
+++ include/sec21/expects.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#if __cpp_lib_source_location >= 201907L
-#include <source_location>
-#endif
 #include <stdexcept>
 #include <string_view>
 
@@ -15,25 +12,11 @@ namespace sec21
    };
 
    template <ErrorHandling Action = ErrorHandling::Throwing, typename Condition>
-#if __cpp_lib_source_location >= 201907L
-   // \todo make use of std::source_location
-   constexpr void expects(Condition condition, std::string_view error_message,
-                          std::source_location location = std::source_location::current())
-#else
    constexpr void expects(Condition condition, std::string_view error_message)
-#endif
    {
       if constexpr (Action == ErrorHandling::Throwing) {
          if (condition() == false) {
-#if __cpp_lib_source_location >= 201907L
-            std::stringstream ss;
-            ss << "file: " << location.file_name() << "(" << location.line() << ":" << location.column() << ") "
-               << std::quoted(location.function_name()) << ": " << error_message;
-
-            throw std::runtime_error(ss.str());
-#else
             throw std::runtime_error(error_message.data());
-#endif
          }
       }
       if constexpr (Action == ErrorHandling::Terminate) {
