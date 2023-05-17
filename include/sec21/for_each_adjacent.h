d3f54ee940c73c8eb3406e99bc086f945b6ee6bc
MichaelMiller- <miller.michael@gmx.de>
2023-05-17 23:16:25 +0200

    consider noexcept functions

--- include/sec21/for_each_adjacent.h
+++ include/sec21/for_each_adjacent.h
@@ -5,7 +5,9 @@
 namespace sec21
 {
    template <typename Iterator, class Callable>
-   auto for_each_adjacent(Iterator first, Iterator last, Callable&& func)
+   auto for_each_adjacent(Iterator first, Iterator last, Callable&& func) noexcept(
+      std::is_nothrow_invocable_v<Callable, typename std::iterator_traits<Iterator>::value_type,
+                                  typename std::iterator_traits<Iterator>::value_type>)
    {
       if (first == last) {
          return;
@@ -20,7 +22,9 @@ namespace sec21
    }
 
    template <typename Sequence, class Callable>
-   auto for_each_adjacent(Sequence&& sequence, Callable&& func)
+   auto for_each_adjacent(Sequence&& sequence, Callable&& func) noexcept(
+      std::is_nothrow_invocable_v<Callable, typename std::decay_t<Sequence>::value_type,
+                                  typename std::decay_t<Sequence>::value_type>)
    {
       return for_each_adjacent(begin(sequence), end(sequence), std::forward<Callable>(func));
    }
