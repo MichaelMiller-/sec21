3c436a8ee9d5f9bdb6648d4a6df715d1f0cba766
MichaelMiller- <miller.michael@gmx.de>
2023-08-05 09:36:54 +0200

    simplify algorithm code and make the operation arguments more obvious

--- tests/zip.cpp
+++ tests/zip.cpp
@@ -5,20 +5,20 @@
 #include <map>
 #include <vector>
 
-TEST_CASE("zip two containers to a map", "[sec21][core]")
-{
-   using namespace sec21;
+using namespace sec21;
 
-   const std::vector<int> keys{ 1, 2, 3 };
+TEST_CASE("zip two containers to a std::map", "[sec21]")
+{
+   const std::vector keys{ 1, 2, 3 };
    const std::vector<std::string> values{ "foo", "bar", "baz" };
 
    std::map<int, std::string> result;
 
-   sec21::zip(
-      [&result](auto&& tuple) { result[std::get<0>(tuple)] = std::get<1>(tuple); }, 
-      std::begin(keys), 
-      std::end(keys), 
-      std::begin(values));   
+   zip(
+      [&result](auto key, auto value) { result[key] = value; },
+      begin(keys),
+      end(keys),
+      begin(values));
 
    const decltype(result) expected{
       { 1, "foo" },
@@ -26,31 +26,53 @@ TEST_CASE("zip two containers to a map", "[sec21][core]")
       { 3, "baz" },
    };
 
-   REQUIRE(std::size(result) == std::size(expected));
-   REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)));
+   REQUIRE(size(result) == size(expected));
+   REQUIRE(std::equal(begin(result), end(result), begin(expected)));
 }
 
-TEST_CASE("zip three sequences into one", "[sec21][core]")
+TEST_CASE("zip three sequences into one", "[sec21]")
 {
-   const std::vector<int> input1{ 1, 4, 7 };
-   const std::vector<int> input2{ 2, 5, 8 };
-   const std::vector<int> input3{ 3, 6, 9 };
+   const std::vector input1{ 1, 4, 7 };
+   const std::vector input2{ 2, 5, 8 };
+   const std::vector input3{ 3, 6, 9 };
 
    std::vector<int> result;
 
-   sec21::zip(
-      [&result](auto&& tuple) { 
-         result.push_back(std::get<0>(tuple));
-         result.push_back(std::get<1>(tuple));
-         result.push_back(std::get<2>(tuple));
+   zip(
+      [&result](auto v1, auto v2, auto v3) {
+         result.push_back(v1);
+         result.push_back(v2);
+         result.push_back(v3);
       },
-      std::begin(input1), 
-      std::end(input1), 
-      std::begin(input2),
-      std::begin(input3));   
+      begin(input1),
+      end(input1),
+      begin(input2),
+      begin(input3));
 
    const decltype(result) expected{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-   REQUIRE(std::size(result) == std::size(expected));
-   REQUIRE(std::equal(std::begin(result), std::end(result), std::begin(expected)));   
+   REQUIRE(size(result) == size(expected));
+   REQUIRE(std::equal(begin(result), end(result), begin(expected)));
+}
+
+TEST_CASE("zip two unequal sequences where the second sequence is longer", "[sec21]")
+{
+   const std::vector input1{ 1, 4, 7 };
+   const std::vector input2{ 1, 2, 3, 4, 5 };
+
+   std::vector<int> result;
+
+   zip(
+      [&result](auto v1, auto v2) {
+         result.push_back(v1);
+         result.push_back(v2);
+      },
+      begin(input1),
+      end(input1),
+      begin(input2));
+
+   const decltype(result) expected{ 1, 1, 4, 2, 7, 3 };
+
+   REQUIRE(size(result) == size(expected));
+   REQUIRE(std::equal(begin(result), end(result), begin(expected)));
 }
