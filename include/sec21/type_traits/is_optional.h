e28839dbc2c5d7becfd84ed51e45d898dc90fdb2
MichaelMiller- <miller.michael@gmx.de>
2024-07-05 22:21:19 +0200

    add some new type traits

---
+++ include/sec21/type_traits/is_optional.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <optional>
+
+namespace sec21::type_traits
+{
+   template <typename T, typename = void>
+   struct is_optional : std::false_type
+   {
+   };
+
+   template <typename T>
+   struct is_optional<std::optional<T>, void> : std::true_type
+   {
+   };
+
+   template <typename T>
+   inline constexpr auto is_optional_v = is_optional<T>::value;
+
+} // namespace sec21::type_traits
